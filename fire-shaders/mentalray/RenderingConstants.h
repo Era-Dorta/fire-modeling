2d2ce296405799e02591f9f98db0f7e75edde02b
Garoe <garoedp@gmail.com>
2015-07-02 11:30:11 +0100

    Fire shader, bb_radiation, added constans in nanometres
    The unit for the constans is controlled by a macro definition

--- fire-shaders/mentalray/RenderingConstants.h
+++ fire-shaders/mentalray/RenderingConstants.h
@@ -10,6 +10,10 @@
 
 #include <cmath>
 
+// Define to have the constant in nanometres, left undefined to have the
+// Constants in SI units
+#define BB_IN_NANOMETRES
+
 // Data from Optical Constants of Soot and Their Application to Heat-Flux
 // Calculations, 1969
 namespace Soot {
@@ -36,12 +40,17 @@ const static miScalar PI_R3_36 = (4.0 / 3.0) * M_PI * R * R * R * 36 * M_PI;
 // Calculations, 1969
 namespace BB {
 const static unsigned num_samples = 30;
-const static miScalar k = 1.3806488e-23; // Bolztmann constant in J/K
-//const static miScalar k = 8.6173324e-5; // Bolztmann constant in eV/K
-//const static miScalar k = 1.3806488e-16; // Bolztmann constant in erg/K
-const static miScalar h = 6.62606957e-34; // Planck constant in J/s
-const static miScalar c0 = 299792458; // Speed of light m/s
+#ifdef BB_IN_NANOMETRES
+const static miScalar k = 1.3806488e-5; // Bolztmann constant in (kg nm^2)/s^2
+const static miScalar h = 6.62606957e-16; // Planck constant in (kg nm^2)/s^2
+const static miScalar c0 = 299792458e9; // Speed of light in nm/s
 const static miScalar two_h = 2 * h;
+#else
+const static miScalar k = 1.3806488e-23; // Bolztmann constant in J/K
+const static miScalar h = 6.62606957e-34;// Planck constant in J/s
+const static miScalar c0 = 299792458;// Speed of light m/s
+const static miScalar two_h = 2 * h;
+#endif
 }
 
 #endif /* RENDERINGCONSTANTS_H_ */
