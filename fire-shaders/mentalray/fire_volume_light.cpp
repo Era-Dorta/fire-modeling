6e5cf896492479d649a895dc21db47a3959424bd
Garoe <garoedp@gmail.com>
2015-07-06 11:58:15 +0100

    Fire shader, cmake, added support for building using cmake

--- fire-shaders/mentalray/fire_volume_light.cpp
+++ fire-shaders/mentalray/fire_volume_light.cpp
@@ -93,7 +93,6 @@ extern "C" DLLEXPORT miBoolean fire_volume_light_exit(miState *state,
 	return miaux_release_user_memory("fire_volume_light", state, params);
 }
 
-
 //TODO Add check for small contribution and return (miBoolean)2
 extern "C" DLLEXPORT miBoolean fire_volume_light(miColor *result,
 		miState *state, struct fire_volume_light *params) {
