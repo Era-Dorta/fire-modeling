8b5f24106641c4f879f5f0042a1ea5470869e3c0
Garoe <garoedp@gmail.com>
2015-08-05 09:22:42 +0100

    Uintah, added width, height, depth parameters to sparse converter

--- pegoraro_data/src/uintahToSparse.cpp
+++ pegoraro_data/src/uintahToSparse.cpp
@@ -25,16 +25,35 @@ void check_out_stream(std::ifstream& in_stream, const std::string &out_file,
 	}
 }
 
+void usage_message() {
+	cout << endl;
+	cout << endl
+			<< "Usage: uintahToSparse <width> <height> <depth> <dense_file> <sparse_file>"
+			<< endl;
+	cout << endl;
+}
+
 int main(int argc, char *argv[]) {
-	if (argc != 3) {
-		cout << endl;
-		cout << endl << "Usage: uintahToSparse <dense_file> <sparse_file>"
-				<< endl;
-		cout << endl;
+	if (argc != 6) {
+		usage_message();
 		exit(0);
 	}
-	const std::string &in_file(argv[1]);
-	const std::string &out_file(argv[2]);
+
+	unsigned width = 0;
+	unsigned height = 0;
+	unsigned depth = 0;
+	try {
+		width = std::stoi(argv[1]);
+		height = std::stoi(argv[2]);
+		depth = std::stoi(argv[3]);
+	} catch (const std::exception& e) {
+		cerr << "Could not parse width = " << argv[1] << ", height = "
+				<< argv[2] << ", depth = " << argv[3] << endl;
+		usage_message();
+		exit(-1);
+	}
+	const std::string &in_file(argv[4]);
+	const std::string &out_file(argv[5]);
 
 	std::ifstream in_stream(in_file.c_str(), std::ios_base::in);
 	if (!in_stream.is_open()) {
@@ -49,17 +68,6 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 	out_stream << std::scientific; // Scientific precision
-
-	unsigned width, height, depth;
-	in_stream >> width;
-	check_in_stream(in_file, in_stream, out_stream);
-
-	in_stream >> height;
-	check_in_stream(in_file, in_stream, out_stream);
-
-	in_stream >> depth;
-	check_in_stream(in_file, in_stream, out_stream);
-
 
 	map<double, int> repeated_count;
 
@@ -81,9 +89,9 @@ int main(int argc, char *argv[]) {
 				in_stream >> read_val;
 				check_in_stream(in_file, in_stream, out_stream);
 
-				if(repeated_count.find(read_val) != repeated_count.end()){
+				if (repeated_count.find(read_val) != repeated_count.end()) {
 					repeated_count.at(read_val) += 1;
-				}else{
+				} else {
 					repeated_count.insert(make_pair(read_val, 1));
 				}
 			}
@@ -94,17 +102,17 @@ int main(int argc, char *argv[]) {
 	double val_max = NAN;
 	int val_max_count = 0;
 
-	if(repeated_count.size() > 0){
+	if (repeated_count.size() > 0) {
 		auto iter = repeated_count.begin();
 
 		// Initialise to first element
-		val_max =  iter->first;
+		val_max = iter->first;
 		val_max_count = iter->second;
 
 		// Compare with the rest in the container
 		iter++;
-		for(; iter != repeated_count.end(); ++iter){
-			if(iter->second >= val_max_count){
+		for (; iter != repeated_count.end(); ++iter) {
+			if (iter->second >= val_max_count) {
 				val_max = iter->first;
 				val_max_count = iter->second;
 			}
@@ -115,18 +123,12 @@ int main(int argc, char *argv[]) {
 	in_stream.clear();
 	in_stream.seekg(0, ios::beg);
 
-	// Advance to the data
-	for (unsigned i = 0; i < 3; i++) {
-		double aux;
-		in_stream >> aux;
-		check_in_stream(in_file, in_stream, out_stream);
-	}
-
 	double background = val_max;
 	int num_valid = width * height * depth - val_max_count;
 
 	// Actual read/write loop
-	out_stream << width << " " << height << " " << depth << " " << num_valid << " " << background << endl;
+	out_stream << width << " " << height << " " << depth << " " << num_valid
+			<< " " << background << endl;
 	check_out_stream(in_stream, out_file, out_stream);
 
 	for (unsigned i = 0; i < width; i++) {
@@ -147,7 +149,8 @@ int main(int argc, char *argv[]) {
 				check_in_stream(in_file, in_stream, out_stream);
 
 				if (read_val != background) {
-					out_stream << x << " " << y << " " << z << " " << read_val << endl;
+					out_stream << x << " " << y << " " << z << " " << read_val
+							<< endl;
 					check_out_stream(in_stream, out_file, out_stream);
 				}
 			}
