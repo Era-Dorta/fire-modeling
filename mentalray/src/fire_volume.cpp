c9f5f83823c3d25143dc3b6114408aa834443ae7
Garoe <garoedp@gmail.com>
2015-08-26 15:22:52 +0100

    Ui_fix, added comments to explain the abundance of attributes in fire_volume

--- mentalray/src/fire_volume.cpp
+++ mentalray/src/fire_volume.cpp
@@ -9,6 +9,9 @@
 
 //#define DEBUG_SIGMA_A
 
+// There are several dummy attributes, that allow for easier control of the UI.
+// Those attributes are copies of the other shader attributes, changing them
+// in this shader then also modifies them in the other shaders.
 struct fire_volume {
 	miColor color;
 	miColor glowColor;
@@ -16,20 +19,20 @@ struct fire_volume {
 	miColor transparency;
 	miTag density_shader;
 	miTag absorption_shader;
-	miTag density_file;
-	miScalar density_scale;
-	miScalar density_offset;
-	miInteger density_read_mode;
-	miTag temperature_file;
-	miScalar temperature_scale;
-	miScalar temperature_offset;
-	miInteger temperature_read_mode;
-	miInteger interpolation_mode;
+	miTag density_file; // Dummy
+	miScalar density_scale; // Dummy
+	miScalar density_offset; // Dummy
+	miInteger density_read_mode; // Dummy
+	miTag temperature_file; // Dummy
+	miScalar temperature_scale; // Dummy
+	miScalar temperature_offset; // Dummy
+	miInteger temperature_read_mode; // Dummy
+	miInteger interpolation_mode; // Dummy
 	miInteger fuel_type;
-	miScalar visual_adaptation_factor;
-	miScalar intensity;
-	miScalar shadow_threshold;
-	miScalar decay;
+	miScalar visual_adaptation_factor; // Dummy
+	miScalar intensity; // Dummy
+	miScalar shadow_threshold; // Dummy
+	miScalar decay; // Dummy
 	miScalar march_increment;
 	miBoolean cast_shadows;
 	miInteger i_light;	// index of first light
