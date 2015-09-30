504bba36c3d167c27391e23e2b538c9797116fcf
Garoe <garoedp@gmail.com>
2015-09-30 12:19:29 +0100

    Im-diff, added simple matlab script that searchs for fire parameters randomly
    As a simple start, computes the mean square error between a target and a
    current image generated with random parameters

---
+++ image_differences/matlab/fire_attr_search.m
@@ -0,0 +1,155 @@
+%% Initialization all
+
+clear all;
+close all;
+
+% N.B. If Matlab is started from the GUI and custom paths are used for the
+% Maya plugins, Matlab will not read the Maya path variables that were
+% defined in the .bashrc file and the render script will fail, a 
+% workouround is to redefine them here:
+% setenv('MAYA_SCRIPT_PATH', 'scripts path');
+% setenv('MI_CUSTOM_SHADER_PATH', ' shaders include path');
+% setenv('MI_LIBRARY_PATH', 'shaders path');
+
+max_ite = 10; % Num of maximum iterations
+epsilon = 100; % Error tolerance
+
+fire_shader_name = 'fire_volume_shader';
+project_path = '~/maya/projects/fire/';
+scene_name = 'test56_like39_ray_march_fix';
+scene_path = [project_path 'scenes/' scene_name '.ma' ];
+output_img_folder = [project_path 'images/' scene_name '/'];
+goal_img_path = [project_path 'images/' scene_name '/test56_like39_ray_march_fix.001.Le.tif'];
+goal_img = imread(goal_img_path);
+
+% Render script is located on the same folder as this file
+[pathToRenderScript,~,~] = fileparts(mfilename('fullpath'));
+pathToRenderScript = [pathToRenderScript '/render-diff.sh'];
+
+% <densityScale> <densityOffset> <temperatureScale> <temperatureOffset> <intensity> <transparency>
+fire_attr = zeros(6, 1);
+
+%% Generate random values for the parameters first step
+% densityScale
+min = 1;
+max = 100;
+fire_attr(1) = min + (max - min) * rand(1);
+
+% densityOffset
+min = 0;
+max = 100;
+fire_attr(2) = min + (max - min) * rand(1);
+
+% temperatureScale
+min = 1;
+max = 1000000;
+fire_attr(3) = min + (max - min) * rand(1);
+
+% temperatureOffset
+min = 1;
+max = 10000;
+fire_attr(4) = min + (max - min) * rand(1);
+
+% intensity
+min = 1;
+max = 100;
+fire_attr(5) = min + (max - min) * rand(1);
+
+% transparency
+min = 1;
+max = 100;
+fire_attr(6) = min + (max - min) * rand(1);
+
+fire_attr
+
+%% Render one image with this parameters
+
+cmdStr = [pathToRenderScript ' ' scene_path ' 0'];
+for i=1:size(fire_attr)
+    cmdStr = [cmdStr ' ' num2str(fire_attr(i))];
+end
+
+if(system(cmdStr) ~= 0)
+    disp(['Render error, check the logs in ' output_img_folder '*.log']);
+    return;
+end
+
+%% Compute the error with respect to the goal image
+c_img = imread([output_img_folder scene_name '0.tif']);
+best_error = sum(MSE(goal_img, c_img));
+% Update to 2014 to use the builtin mse
+% best_error = immse(goal_img, c_img); % MSE of the two images
+best_attr = fire_attr;
+
+%% Main loop auxilary variables initialization
+c_error = best_error;
+c_ite = 1;
+best_ite = 0;
+
+%% Main loop
+while (c_ite < max_ite &&  c_error > epsilon)
+    %##############################################
+    % Generate random values for the parameters
+    %##############################################
+    % densityScale
+    min = 1;
+    max = 100;
+    fire_attr(1) = min + (max - min) * rand(1);
+    
+    % densityOffset
+    min = 0;
+    max = 100;
+    fire_attr(2) = min + (max - min) * rand(1);
+    
+    % temperatureScale
+    min = 1;
+    max = 1000000;
+    fire_attr(3) = min + (max - min) * rand(1);
+    
+    % temperatureOffset
+    min = 1;
+    max = 10000;
+    fire_attr(4) = min + (max - min) * rand(1);
+    
+    % intensity
+    min = 1;
+    max = 100;
+    fire_attr(5) = min + (max - min) * rand(1);
+    
+    % transparency
+    min = 1;
+    max = 100;
+    fire_attr(6) = min + (max - min) * rand(1);
+    
+    fire_attr
+    
+    %##############################################
+    % Render new image
+    %##############################################
+    
+    cmdStr = [pathToRenderScript ' ' scene_path ' ' num2str(c_ite)];
+    for i=1:size(fire_attr)
+        cmdStr = [cmdStr ' ' num2str(fire_attr(i))];
+    end
+    
+    if(system(cmdStr) ~= 0)
+       disp(['Render error, check the logs in ' output_img_folder '*.log']);
+       return;
+    end
+       
+    % Compute error
+    c_img = imread([output_img_folder scene_name num2str(c_ite) '.tif']);
+    c_error = sum(MSE(goal_img, c_img));
+    
+    % Update if we are closer to the image
+    if(c_error < best_error)
+        best_error = c_error;
+        best_attr = fire_attr;
+        best_ite = c_ite;
+    end
+    
+    c_ite = c_ite + 1;
+end
+
+save('fire_attr_search.txt', 'best_error', 'fire_attr', '-ascii');
+disp(['Best image is number ' num2str(best_ite)]);
