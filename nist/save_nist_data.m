4b336434f338b651c1b5933663ed4676bfd191d8
Garoe <garoedp@gmail.com>
2015-11-05 11:25:04 +0000

    Nist, added E1, E2, g1 and g2 values to the specline files
    This data is needed to compute the full equations for exotic chemical
    species

--- nist/save_nist_data.m
+++ nist/save_nist_data.m
@@ -30,22 +30,31 @@ for i=1:size(spec, 2)
         end
     end
     
-    % Save the normalized intensities in a variable with the wavelenghts
-    norm_intensities = zeros(num_valid, 3);
+    % Save the normalized intensities, wavelengths, A21, E1, E2, g1, g2
+    specline_data = zeros(num_valid, 7);
     valid_ind = 1;
     
     for ii=1:length(nistln)
         if ~isempty(nistln(ii).rint) && ~isempty(nistln(ii).Aki)
-            norm_intensities(valid_ind, 1) = nistln(ii).meanor;
-            norm_intensities(valid_ind, 2) = nistln(ii).rint/mxrint;
-            norm_intensities(valid_ind, 3) = nistln(ii).Aki;
+            specline_data(valid_ind, 1) = nistln(ii).meanor;
+            specline_data(valid_ind, 2) = nistln(ii).rint/mxrint;
+            specline_data(valid_ind, 3) = nistln(ii).Aki;
+            specline_data(valid_ind, 4) = nistln(ii).Ei;
+            specline_data(valid_ind, 5) = nistln(ii).Ek;
+            % g1 and g2 are in a string '%d - %d'
+            C = textscan(nistln(ii).gigk,'%d%s%d');
+            specline_data(valid_ind, 6) = C{1};
+            specline_data(valid_ind, 7) = C{3};
             valid_ind = valid_ind + 1;
         end
     end
     
-    % Save both variables into a file
+    % Save the data into a file
     fileID = fopen([file_dest, spec{i}, '.specline'],'w');
+    % First line number of data lines in the file
     fprintf(fileID, '%d\n',num_valid);
-    fprintf(fileID, '%d %d %d\n',norm_intensities');
+    % Each file line corresponds to a visible is a spectrum line:
+    % \lambda \phi A21 E1 E2 g1 g2
+    fprintf(fileID, '%f %f %f %f %f %d %d\n',specline_data');
     fclose(fileID);
 end
