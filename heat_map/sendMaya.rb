2bf389689c8dcabaccab25e9badf7c5475335061
Garoe <garoedp@gmail.com>
2015-11-09 10:30:26 +0000

    Maya-batch, increase robustness in send Maya script
    If the socket can not be open, it waits and retries a couple of times
    before failing

--- heat_map/sendMaya.rb
+++ heat_map/sendMaya.rb
@@ -2,12 +2,26 @@
 
 require 'socket'
 
-begin
+# maxRetries * 0.1 = number of seconds waiting for Maya to load
+maxRetries = 300
+timesFailed = 0
 
-mel = STDIN.read
+while timesFailed <= maxRetries do
+	begin
+		mel = STDIN.read
 
-s = TCPSocket.open("localhost", 2222)
+		s = TCPSocket.open("localhost", 2222)
 
-s.puts(mel)
+		s.puts(mel)
 
+		mayaReturn = s.gets()
+		
+		exit(0)
+	rescue Errno::ECONNREFUSED
+		timesFailed += 1
+		# Maya is not ready yet, wait a bit before retrying
+		sleep(0.1)
+	end
 end
+
+exit(-1)
