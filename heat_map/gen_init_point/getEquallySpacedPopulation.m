b7f1e46a38c81503402df540f03b2079507971a8
Garoe <garoedp@gmail.com>
2015-11-23 12:05:40 +0000

    GA added comment to get*Population functions

--- heat_map/gen_init_point/getEquallySpacedPopulation.m
+++ heat_map/gen_init_point/getEquallySpacedPopulation.m
@@ -1,6 +1,9 @@
 function [ result ] = getEquallySpacedPopulation( LB, UB, n )
 % Generates a new population of size n in the LB and UB range, of uniformly
 % space individuals where each of them has the same value for all dimensions
+
+% This function is meant to be used to initialize the
+% options.InitialPopulation matrix in the GA solver
 myfoo = @(x, y)linspace(x , y, n)';
 result = cell2mat(arrayfun(myfoo, LB, UB, 'Uniform', false));
 end
--- heat_map/gen_init_point/getRandomInitPopulation.m
+++ heat_map/gen_init_point/getRandomInitPopulation.m
@@ -1,6 +1,9 @@
 function [ result ] = getRandomInitPopulation( LB, UB, n )
 % Generates a new population of size n in the LB and UB range of randomly
 % selected individuals
+
+% This function is meant to be used to initialize the
+% options.InitialPopulation matrix in the GA solver
 result = rand(n, size(LB, 2));
 for i=1:n
     result(i,:) = fitToRange(result(i,:), 0, 1, LB, UB);
