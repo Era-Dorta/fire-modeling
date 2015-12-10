3785ad5bbe79a7b843a4414ce4cb9441598fc455
Garoe <garoedp@gmail.com>
2015-12-10 15:41:03 +0000

    Mex files, compile script robustness
    It now works when called outside of its folder

--- heat_map/mex_fnc/compileAll.m
+++ heat_map/mex_fnc/compileAll.m
@@ -1,3 +1,10 @@
 % Script that compiles all the files in the mex_fnc folder
 
+% Go into current folder as we are giving mex relative file paths
+cdir = pwd;
+cd(fileparts(mfilename('fullpath')));
+
 mex mixHeatMaps.cpp createVoxelDataSet.cpp -lopenvdb -lHalf -ltbb  -L/usr/lib/x86_64-linux-gnu/ -L/usr/include/
+
+% Return to previous folder
+cd(cdir); 
