7accfc6ea5a6db26d35c85df48ac109debf31a00
Garoe <garoedp@gmail.com>
2015-12-11 14:01:57 +0000

    ovsb2mat, improved compileAll script
    Using variables for future crossplatform compatibility and other
    extensions

--- heat_map/mex_fnc/compileAll.m
+++ heat_map/mex_fnc/compileAll.m
@@ -4,7 +4,31 @@
 cdir = pwd;
 cd(fileparts(mfilename('fullpath')));
 
-mex mixHeatMaps.cpp createVoxelDataSet.cpp -lopenvdb -lHalf -ltbb  -L/usr/lib/x86_64-linux-gnu/ -L/usr/include/
+% The name of each .cpp file that we would like to compile
+fncNames = {'mixHeatMaps.cpp'};
+
+% Current folder depedencies source code of the functions, separated by spaces
+commonSrc = 'createVoxelDataSet.cpp';
+
+% Libraries header folders, separated by spaces
+libs_inc = '-L/usr/include/';
+
+% Linking libraries, separated by spaces
+libs = '-lopenvdb -lHalf -ltbb';
+
+% Libraries search path
+libs_path = '-L/usr/lib/x86_64-linux-gnu/';
+
+
+for i = 1:numel(fncNames)
+    % Clear all the functions from memory, if we are compiling after a change
+    % in the code, this ensures that the new version will be used
+    [~, nameNoExt, ~] = fileparts(fncNames{i});
+    clear(nameNoExt);
+
+    % Compile current function
+    mex(fncNames{i}, commonSrc, libs, libs_path, libs_inc);
+end
 
 % Return to previous folder
 cd(cdir); 
