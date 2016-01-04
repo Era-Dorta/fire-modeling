9bd37f98219465ee14dc675aed1c137b227b15fe
Garoe <garoedp@gmail.com>
2016-01-04 17:57:28 +0000

    ovdb2mat, added help file for mixHeatMaps function

---
+++ heat_map/mex_fnc/mixHeatMaps.m
@@ -0,0 +1,11 @@
+function [V, XYZ] = mixHeatMaps(XYZ0, V0, V1, MIN, MAX)
+%mixHeatMaps Mixes two heat maps
+%   [V, XYZ] = mixHeatMaps(XYZ0, V0, V1, MIN, MAX) returns the values after
+%   dividing two heatmaps in 8 cubes, taking 4 cubes from each of them and
+%   interpolating the values in the intersections. XYZ0 is a Mx3 matrix of 
+%   coordinates for each V data, V0, V1 are column vectors of size M of
+%   volume values, MIN and MAX are row vectors with the min and max 
+%   [x, y, z] coordinates of XYZ0, V is a column vector of size M with the
+%   output values and XYZ is a Mx3 matrix of coordinates for each V data.
+
+
