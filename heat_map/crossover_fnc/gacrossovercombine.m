373e7e0b0bcd11799fb0bd05553385d17d432369
Garoe <garoedp@gmail.com>
2016-01-05 10:08:15 +0000

    ovdb2mat, added skeleton for custom crossover function

---
+++ heat_map/crossover_fnc/gacrossovercombine.m
@@ -0,0 +1,10 @@
+function [ xoverKids ] = gacrossovercombine(parents, options, nvars, FitnessFcn, ...
+    unused,thisPopulation)
+%GACROSSOVERCOMBINE ga crossover operator
+%   GACROSSOVERCOMBINE uses the combineHeatMap8 function to generate 
+%   xoverKids from parents
+%
+%   See also COMBINEHEATMAP8.
+
+end
+
