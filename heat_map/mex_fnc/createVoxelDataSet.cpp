ae70a57ec9d0f4705271b4ec788ad60feaddc3cc
Garoe <garoedp@gmail.com>
2016-01-05 14:56:42 +0000

    crossover, added new function to convert from ovdb to matlab
    The new function outputs the values given by some coordinates in the
    same order

--- heat_map/mex_fnc/createVoxelDataSet.cpp
+++ heat_map/mex_fnc/createVoxelDataSet.cpp
@@ -29,7 +29,7 @@ void array2voxelDataset(const mxArray valuesMx[], const mxArray coordsMx[],
 		mexErrMsgTxt("Values and indices must be the same row size.");
 	}
 
-	for (int i = 0; i < valSize0 * valSize1; i++) {
+	for (int i = 0; i < valSize0; i++) {
 		// Matlab stores the data in column order, so to access the (i,j)
 		// component we need to compute i + j * M
 		openvdb::Coord c(coord[i], coord[i + valSize0],
@@ -78,5 +78,36 @@ void voxelDatasetValues2array(openvdb::FloatGrid::ConstPtr inGrid,
 			iter.test(); ++iter) {
 		values[i] = *iter;
 		i++;
+	}
+}
+
+void voxelDatasetValues2arrayOrdered(openvdb::FloatGrid::ConstPtr inGrid,
+		const mxArray coordsMx[], mxArray *valuesMx[]) {
+	const int size = inGrid->activeVoxelCount();
+	openvdb::FloatGrid::ConstAccessor accessor = inGrid->getAccessor();
+
+	const int coordSize0 = mxGetM(coordsMx);
+	const int coordSize1 = mxGetN(coordsMx);
+
+	double *coord = mxGetPr(coordsMx);
+
+	if (coordSize1 != 3) {
+		mexErrMsgTxt("Indices must be threedimensional.");
+	}
+
+	if (coordSize0 != size) {
+		mexErrMsgTxt("Values and indices must be the same row size.");
+	}
+
+	valuesMx[0] = mxCreateDoubleMatrix(size, 1, mxREAL);
+
+	double *values = mxGetPr(valuesMx[0]);
+
+	for (int i = 0; i < size; i++) {
+		// Matlab stores the data in column order, so to access the (i,j)
+		// component we need to compute i + j * M
+		openvdb::Coord c(coord[i], coord[i + coordSize0],
+				coord[i + 2 * coordSize0]);
+		values[i] = accessor.getValue(c);
 	}
 }
--- heat_map/mex_fnc/createVoxelDataSet.h
+++ heat_map/mex_fnc/createVoxelDataSet.h
@@ -5,13 +5,30 @@
 
 #include <openvdb/openvdb.h>
 
+/*
+ * Store in outGrid the voxel dataset defined by the values in valuesMx in
+ * the coordinates coordsMx
+ */
 void array2voxelDataset(const mxArray valuesMx[], const mxArray coordsMx[],
 		openvdb::FloatGrid::Ptr outGrid);
 
+/*
+ * Store in valuesMx and coordsMx the voxel dataset grid inGrid
+ */
 void voxelDataset2array(openvdb::FloatGrid::ConstPtr inGrid,
 		mxArray *valuesMx[], mxArray *coordsMx[]);
 
+/*
+ * Store in valuesMx the voxel dataset values of grid inGrid
+ */
 void voxelDatasetValues2array(openvdb::FloatGrid::ConstPtr inGrid,
 		mxArray *valuesMx[]);
+
+/*
+ * Store in valuesMx the voxel dataset values defined by the coordinates
+ * coordsMx in inGrid
+ */
+void voxelDatasetValues2arrayOrdered(openvdb::FloatGrid::ConstPtr inGrid,
+		const mxArray coordsMx[], mxArray *valuesMx[]);
 
 #endif /* CREATEVOXELDATASET_H_ */
