95af17755987546cc065022b76a029da42082b36
Garoe <garoedp@gmail.com>
2016-01-05 10:07:57 +0000

    ovdb2mat, minor fixes in help text

--- heat_map/mex_fnc/combineHeatMap8.m
+++ heat_map/mex_fnc/combineHeatMap8.m
@@ -1,11 +1,9 @@
-function [V] = combineHeatMap8(XYZ, V0, V1, MIN, MAX)
-%combineHeatMap8 Combines two heat maps
-%   [V] = combineHeatMap8(XYZ, V0, V1, MIN, MAX) returns the values after
+%COMBINEHEATMAP8 Combines two heat maps
+%   [V] = COMBINEHEATMAP8(XYZ, V0, V1, MIN, MAX) returns the values after
 %   dividing two heatmaps in 8 cubes, taking 4 cubes from each of them and
-%   interpolating the values in the intersections. XYZ is a Mx3 matrix of 
+%   interpolating the values in the intersections. XYZ is a Mx3 matrix of
 %   coordinates for each V data, V0, V1 are column vectors of size M of
-%   volume values, MIN and MAX are row vectors with the min and max 
+%   volume values, MIN and MAX are row vectors with the min and max
 %   [x, y, z] coordinates of XYZ, V is a column vector of size M with the
 %   output values.
-
-
+%
--- heat_map/mutation_fnc/gamutationmean.m
+++ heat_map/mutation_fnc/gamutationmean.m
@@ -1,10 +1,12 @@
 function [ mutationChildren ] = gamutationmean(parents, options, nvars, ...
     FitnessFcn, state, thisScore, thisPopulation)
-% Ga mutation function that generates mutations using the voxel means
-%   Any mutation that is generated will not exceed the range of 
+%GAMUTATIONMEAN is a ga mutation function that generates mutations using the voxel means
+%   [ mutationChildren ] = GAMUTATIONMEAN(parents, options, nvars, ...
+%   FitnessFcn, state, thisScore, thisPopulation)
+%   Any mutation that is generated will not exceed the range of
 %   [m - min ... m + max] where m is the mean of the values of the
 %   neighbours of the voxel and min and max are user defined parameters
-    options
-    mutationChildren = thisPopulation;
+options
+mutationChildren = thisPopulation;
 end
 
