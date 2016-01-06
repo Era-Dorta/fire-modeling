acc6085b1b16e5eacda783de397e58c3a2371369
Garoe <garoedp@gmail.com>
2016-01-06 10:51:09 +0000

    Crossover, updated combineHeatMap8Test for randomness
    The tests now take into account that the combine function could have
    mixed the heatmaps in a different order

--- heat_map/test/combineHeatMap8Test.m
+++ heat_map/test/combineHeatMap8Test.m
@@ -31,7 +31,7 @@ xyz = [0 0 0;
     2 2 1;
     2 2 2];
 
-%% Test expected output with a 3x3 heatmap with the same values
+%% Test expected output with 3x3 heatmaps with the same values
 
 % Create two random 3x3 heatmaps
 v0 = rand(27, 1);
@@ -43,7 +43,7 @@ v = combineHeatMap8(xyz, v0, v1, min(xyz, [], 1), max(xyz, [], 1));
 % Test that each voxel belongs to the corresponding heatmap or mean of them
 assert(all(abs(v - v0) < tol), 'combineHeatMap8 does not reproduce heatmap coordinates order');
 
-%% Test expected output with a 3x3 heatmap
+%% Test expected output with 3x3 heatmaps
 
 % Create two random 3x3 heatmaps
 v0 = rand(27, 1);
@@ -63,6 +63,9 @@ idxmean([idxv0, idxv1]) = [];
 v = combineHeatMap8(xyz, v0, v1, min(xyz, [], 1), max(xyz, [], 1));
 
 % Test that each voxel belongs to the corresponding heatmap or mean of them
-assert(all(abs(v(idxv0) - v0(idxv0)) < tol), 'combineHeatMap8 wrong values for first grid');
-assert(all(abs(v(idxv1) - v1(idxv1)) < tol), 'combineHeatMap8 wrong values for second grid');
+% As there are two randomly choosen ways to combine the heatmaps, test both
+first_case = all(abs(v(idxv0) - v0(idxv0)) < tol) && all(abs(v(idxv1) - v1(idxv1)) < tol);
+second_case = all(abs(v(idxv1) - v0(idxv1)) < tol) && all(abs(v(idxv0) - v1(idxv0)) < tol);
+
+assert(first_case || second_case, 'combineHeatMap8 wrong values inside the bounding boxes');
 assert(all(abs(v(idxmean) - vmean(idxmean)) < tol), 'combineHeatMap8 wrong values in the boundaries');
