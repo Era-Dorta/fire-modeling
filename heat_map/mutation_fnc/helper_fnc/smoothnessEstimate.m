1f04de352fd19757eb5ad81345d7c832a479dbac
Garoe <garoedp@gmail.com>
2016-01-07 16:11:54 +0000

    mutation, refactored smoothnessEstimate function
    Added size checks for the inputs and variable renaming

--- heat_map/mutation_fnc/helper_fnc/smoothnessEstimate.m
+++ heat_map/mutation_fnc/helper_fnc/smoothnessEstimate.m
@@ -7,28 +7,31 @@ function smoothness = smoothnessEstimate( xyz, v, volumeSize )
 %   SMOOTH_V means high smoothness and a large values indicate low
 %   smoothness.
 
-num_v = size(v, 1);
+num_vol = size(v, 1);
 
-smoothness = zeros(1, num_v);
+smoothness = zeros(1, num_vol);
 
 % Add one to each index to account for the front padding
 xyz = xyz + 1;
 volumeSize = volumeSize + 2;
 
-for i=1:num_v
-    % Get a dense copy of V,
-    V = zeros(volumeSize(1), volumeSize(2), volumeSize(3));
-    vInd = sub2ind(volumeSize, xyz(:,1), xyz(:,2), xyz(:,3));
-    V(vInd) = v(i,:);
-    
-    % Compute the mean for each valid voxel using its Moore neighbor
-    % voxels, one voxel on each direction including the diagonals
-    Vmean = arrayfun(@(i) mean(reshape(V(xyz(i,1)-1:xyz(i,1)+1, ...
-        xyz(i,2)-1:xyz(i,2)+1, xyz(i,3)-1:xyz(i,3)+1), 1, 27)), 1:size(xyz,1));
-    
-    % Compute the mean square error between the both volumes
-    error = Vmean - v(i,:);
-    smoothness(i) = sum(sum(error .* error)) / size(xyz,1);
+for i=1:num_vol
+    num_active = size(v, 2);
+    if(num_active > 0)
+        % Get a dense copy of V,
+        V = zeros(volumeSize(1), volumeSize(2), volumeSize(3));
+        vInd = sub2ind(volumeSize, xyz(:,1), xyz(:,2), xyz(:,3));
+        V(vInd) = v(i,:);
+        
+        % Compute the mean for each valid voxel using its Moore neighbor
+        % voxels, one voxel on each direction including the diagonals
+        Vmean = arrayfun(@(i) mean(reshape(V(xyz(i,1)-1:xyz(i,1)+1, ...
+            xyz(i,2)-1:xyz(i,2)+1, xyz(i,3)-1:xyz(i,3)+1), 1, 27)), 1:num_active);
+        
+        % Compute the mean square error between both volumes
+        error = Vmean - v(i,:);
+        smoothness(i) = sum(sum(error .* error)) / num_active;
+    end
 end
 
 end
