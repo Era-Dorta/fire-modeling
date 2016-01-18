14d95251c40f6fb6ad1c09ec97f69c87fe6f75e1
Garoe <garoedp@gmail.com>
2016-01-18 17:08:58 +0000

    HistogramError, minor refactoring
    The final error computation is now done in one line with less
    intermediate operations

--- heat_map/error_fnc/histogramErrorOpti.m
+++ heat_map/error_fnc/histogramErrorOpti.m
@@ -4,7 +4,7 @@ function [ cerror ] = histogramErrorOpti( goal_im, imga )
 %   version of HISTOGRAM_ERROR, assumes RGB images, ignores black pixels
 %   and if the goal image changes, call clear 'histogramErrorOpti';
 
-persistent N_GOAL NUM_PIXELS_INV
+persistent N_GOAL NORM_FACTOR
 
 % Create 256 bins, image can be 0..255
 edges = linspace(0, 255, 256);
@@ -14,7 +14,8 @@ if isempty(N_GOAL)
     N_GOAL(2, :) = histcounts( goal_im(:, :, 2), edges);
     N_GOAL(3, :) = histcounts( goal_im(:, :, 3), edges);
     
-    NUM_PIXELS_INV = 1 / (size(goal_im, 1) * size(goal_im, 2));
+    % Normalization factor is the inverse of the number of pixels
+    NORM_FACTOR = 1 / numel(goal_im);
 end
 
 % Compute the histogram count for each color channel
@@ -26,9 +27,8 @@ Na(3, :) = histcounts( imga(:, :, 3), edges);
 bin_range = 1:size(Na, 2);
 
 % Compute the error as in Dobashi et. al. 2012
-cerror = sum(abs(Na(1, bin_range) - N_GOAL(1, bin_range))) * NUM_PIXELS_INV;
-cerror = cerror + sum(abs(Na(2, bin_range) - N_GOAL(2, bin_range))) * NUM_PIXELS_INV;
-cerror = cerror + sum(abs(Na(3, bin_range) - N_GOAL(3, bin_range))) * NUM_PIXELS_INV;
+cerror = (sum(abs(Na(1, bin_range) - N_GOAL(1, bin_range))) + ...
+    sum(abs(Na(2, bin_range) - N_GOAL(2, bin_range))) + ...
+    sum(abs(Na(3, bin_range) - N_GOAL(3, bin_range)))) * NORM_FACTOR;
 
-cerror = cerror / 3;
 end
