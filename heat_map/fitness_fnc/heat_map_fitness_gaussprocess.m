6b80eb95bb5ed15c99d5e6f683cdc229c38858ff
Garoe <garoedp@gmail.com>
2016-01-21 13:00:49 +0000

    Render-approx, using larger dataset
    Increased dataset size from 1000 to 10000

--- heat_map/fitness_fnc/heat_map_fitness_gaussprocess.m
+++ heat_map/fitness_fnc/heat_map_fitness_gaussprocess.m
@@ -17,7 +17,7 @@ if isempty(IS_INITIALIZED) || IS_INITIALIZED == false
     assert(isequal(error_foo{1}, @histogramError) || isequal(error_foo{1}, @histogramErrorOpti), ...
         'Only histogram error functions are allowed');
     
-    load('~/maya/projects/fire/images/test78_like_72_4x4x4_raw/render_approx_data0/data.mat');
+    load('~/maya/projects/fire/images/test78_like_72_4x4x4_raw/render_approx_data2/data.mat');
     
     % Heatmaps are already in the right format
     % X = heat_maps
