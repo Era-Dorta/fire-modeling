1cbe351ef7842cd907370af0888bcf1558849b78
Garoe <garoedp@gmail.com>
2016-01-25 13:51:31 +0000

    Render-approx, minor fix in gauss test script

--- heat_map/test/heat_map_fitness_gaussprocessTest.m
+++ heat_map/test/heat_map_fitness_gaussprocessTest.m
@@ -6,12 +6,12 @@
 tol = 0.00001;
 error_th = 10; % No more than 10 histocounts wrong
 
-data_folder = '~/maya/projects/fire/images/test78_like_72_4x4x4_raw/render_approx_data2';
+data_folder = '~/maya/projects/fire/images/test78_like_72_4x4x4_raw/render_approx_data0';
 
 % Load the train data
 train_data_path = [data_folder '/data.mat'];
 if(exist(train_data_path,'file'))
-    load(train_data_path);
+    train = load(train_data_path);
 else
     error(['File ' train_data_path ' not found']);
 end
@@ -19,7 +19,7 @@ end
 % Load the test data
 test_data_path = [data_folder '/test-data.mat'];
 if(exist(test_data_path,'file'))
-    load(test_data_path);
+    test = load(test_data_path);
 else
     error(['File ' test_data_path ' not found']);
 end
@@ -33,20 +33,20 @@ else
 end
 
 % Flatten out the histograms
-Y = cellfun(@(x) [x(1, bin_range), x(2, bin_range), x(3, bin_range)], ...
-    histocounts, 'UniformOutput', false);
+Y = cellfun(@(x) [x(1, train.bin_range), x(2, train.bin_range), x(3, train.bin_range)], ...
+    train.histocounts, 'UniformOutput', false);
 Y = cell2mat(Y);
 
-test_Y = cellfun(@(x) [x(1, bin_range), x(2, bin_range), x(3, bin_range)], ...
-    test_histocounts, 'UniformOutput', false);
+test_Y = cellfun(@(x) [x(1, test.bin_range), x(2, test.bin_range), x(3, test.bin_range)], ...
+    test.histocounts, 'UniformOutput', false);
 test_Y = cell2mat(test_Y);
 
 %% Test with train data
 
-pred_histo = GP.PredictNoChecks(heat_maps);
+pred_histo = GP.PredictNoChecks(train.heat_maps);
 
-pred_error = zeros(size(heat_maps, 1), 1);
-for i=1:size(heat_maps, 1)
+pred_error = zeros(size(train.heat_maps, 1), 1);
+for i=1:size(train.heat_maps, 1)
     pred_error(i) = norm(pred_histo(i, :) - Y(i, :));
 end
 
@@ -54,10 +54,10 @@ assert(mean(pred_error) < error_th, 'Failed with train data');
 
 %% Test with test data
 
-pred_histo = GP.PredictNoChecks(test_heat_maps);
+pred_histo = GP.PredictNoChecks(test.heat_maps);
 
-pred_error = zeros(size(test_heat_maps, 1), 1);
-for i=1:size(test_heat_maps, 1)
+pred_error = zeros(size(test.heat_maps, 1), 1);
+for i=1:size(test.heat_maps, 1)
     pred_error(i) = norm(pred_histo(i, :) - test_Y(i, :));
 end
 
