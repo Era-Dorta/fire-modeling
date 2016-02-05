7f8884ef5104549c71486ba059bb8e9287a7699b
Garoe <garoedp@gmail.com>
2016-02-05 16:52:28 +0000

    varianceTests, increased variance test folder number
    New tests were ran

--- heat_map/output_plot_fnc/varianceTestsPlot.m
+++ heat_map/output_plot_fnc/varianceTestsPlot.m
@@ -3,20 +3,21 @@ close all;
 
 % Plot a comparison between different variance tests results
 %% Initialization
-variance_test_num = [0, 1];
+variance_test_name = {'XoverDefault, MutNone', 'XoverPrior, MutNone', ...
+    'XoverPrior, MutDefault', 'XoverPrior, MutPrior'};
 hm_number = 0:11;
 
 result_path = '~/maya/projects/fire/images/test79_like_78_rot/VarianceTest';
 
-num_var_test = length(variance_test_num);
+num_var_test = length(variance_test_name);
 num_hm = length(hm_number);
 
 %% Read the data
 X = zeros(num_hm, num_var_test);
 Y = zeros(num_hm, num_var_test);
 
-for i=1:length(variance_test_num)
-    istr = num2str(variance_test_num(i));
+for i=1:length(variance_test_name)
+    istr = num2str(i - 1);
     for j=1:length(hm_number)
         T = load([result_path istr '/hm_search_' num2str(hm_number(j)) '/summary_file.mat']);
         X(j,i) = T.summary_data.creation_fnc_sigma;
@@ -38,9 +39,9 @@ end
 figure;
 bar(Y);
 set(gca,'xticklabel',Xstr)
-title('Crossover function comparison');
+title('GA function comparison');
 ylabel('Error function');
 xlabel('Initial Guess Perturbation Variance');
-legend('CrossoverCombinePrior','CrossoverScattered', 'Location', 'northwest')
+legend(variance_test_name, 'Location', 'northwest')
 
-% saveas(gca, 'croosoverComparison.svg', 'svg');
+% saveas(gca, 'GAComparison.svg', 'svg');
