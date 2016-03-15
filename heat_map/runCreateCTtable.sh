157c6ba8596a691e327da2b6d3e5631e4cfb84e1
Garoe <garoedp@gmail.com>
2016-03-15 10:29:33 +0000

    ct_table, added bash launch script for createCTtable function

---
+++ heat_map/runCreateCTtable.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+if [ "$#" -gt 0 ]; then
+	echo ""
+	echo "Too many input arguments"
+	echo ""
+	echo "Usage: runCreateCTtable.sh"
+	echo ""
+	exit 2
+fi
+
+# Maya will listen to this port
+PORT="2222"
+
+# Create random name for the log file to avoid clashes with other matlab logs
+LOGFILE=`mktemp matlabXXXXXXXXXXXXXXXXXXXXX.log`
+LOGFILE=`pwd`"/"$LOGFILE
+
+# Get this script path
+CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Launch Maya
+nice -n20 "$CDIR/runMayaBatch.sh" "$PORT" > /dev/null
+
+if [ "$?" -ne 0 ]; then
+	echo "Could not launch Maya:${PORT}"
+	exit 2
+fi
+
+echo "Launched Maya:${PORT}"
+
+# Runs matlab in batch mode with low priority
+nice -n20 matlab -nodesktop -nosplash -r "createCTtable($PORT, '$LOGFILE')" -logfile $LOGFILE
+
+# Close Maya
+"$CDIR/maya_comm/sendMaya.rb" $PORT "quit -f"
+if [ "$?" -ne 0 ]; then
+	echo "Could not close Maya:$PORT"
+else
+	echo "Closed Maya:$PORT"
+fi
+
