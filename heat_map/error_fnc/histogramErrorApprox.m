2d5bd90f0d23e91fe0bd96a9f04f199ed2fba407
Garoe <garoedp@gmail.com>
2016-03-17 16:53:16 +0000

    ct_prior, fixed multiple goal images error

--- heat_map/error_fnc/histogramErrorApprox.m
+++ heat_map/error_fnc/histogramErrorApprox.m
@@ -5,25 +5,36 @@ function histoE = histogramErrorApprox( v, goal_img, goal_mask)
 %   and PxQ RGB of image and GOAL_MASK is a PxQ logical matrix used as a
 %   mask for GOAL_IMG, HISTOE is the error value
 
-persistent CTtable GoalHisto
+persistent CTtable GoalHisto NumGoal
+
+% Create 256 bins, image can be 0..255
+edges = linspace(0, 255, 256);
 
 if(isempty(CTtable))
     code_dir = fileparts(fileparts(mfilename('fullpath')));
     CTtable = load([code_dir '/data/CT-BlackBody.mat'], '-ascii');
     
-    % Create 256 bins, image can be 0..255
-    edges = linspace(0, 255, 256);
-    GoalHisto = zeros(3, 255);
+    if(~iscell(goal_img))
+        goal_img = {goal_img};
+        goal_mask = {goal_mask};
+    end
     
-    sub_img = goal_img(:, :, 1);
-    GoalHisto(1, :) = histcounts( sub_img(goal_mask), edges);
-    sub_img = goal_img(:, :, 2);
-    GoalHisto(2, :) = histcounts( sub_img(goal_mask), edges);
-    sub_img = goal_img(:, :, 3);
-    GoalHisto(3, :) = histcounts( sub_img(goal_mask), edges);
+    NumGoal = numel(goal_img);
+    GoalHisto = cell(NumGoal, 1);
     
-    % Normalize by the number of pixels
-    GoalHisto = GoalHisto ./ sum(goal_mask(:) == 1);
+    for i=1:NumGoal
+        GoalHisto{i} = zeros(3, 255);
+        
+        sub_img = goal_img{i}(:, :, 1);
+        GoalHisto{i}(1, :) = histcounts( sub_img(goal_mask{i}), edges);
+        sub_img = goal_img{i}(:, :, 2);
+        GoalHisto{i}(2, :) = histcounts( sub_img(goal_mask{i}), edges);
+        sub_img = goal_img{i}(:, :, 3);
+        GoalHisto{i}(3, :) = histcounts( sub_img(goal_mask{i}), edges);
+        
+        % Normalize by the number of pixels
+        GoalHisto{i} = GoalHisto{i} ./ sum(goal_mask{i}(:) == 1);
+    end
 end
 
 % interp_method = 'nearest'; % C0
@@ -32,7 +43,7 @@ interp_method = 'linear'; % C0
 % interp_method = 'spline'; % C2
 
 num_vol = size(v, 1);
-histoV = zeros(1, num_vol);
+histoE = zeros(1, num_vol);
 num_temp_inv = 1.0 / numel(v(1, :));
 
 histo_est = zeros(3, 255);
@@ -51,9 +62,14 @@ for i=1:num_vol
     histo_est(2, :) = histcounts( colors_est(:,2), edges) * num_temp_inv;
     histo_est(3, :) = histcounts( colors_est(:,3), edges) * num_temp_inv;
     
-    histoV(i) = (sum(abs(histo_est(1, :) - GoalHisto(1, :))) +  ...
-        sum(abs(histo_est(2, :) - GoalHisto(2, :))) + ...
-        sum(abs(histo_est(3, :) - GoalHisto(3, :)))) / 6;
+    for j=1:NumGoal
+        histoE(i) = histoE(i) + ...
+            (sum(abs(histo_est(1, :) - GoalHisto{j}(1, :))) +  ...
+            sum(abs(histo_est(2, :) - GoalHisto{j}(2, :))) + ...
+            sum(abs(histo_est(3, :) - GoalHisto{j}(3, :))));
+    end
 end
+
+histoE = histoE ./ (NumGoal * 6);
 
 end
