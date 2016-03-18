6742808f5fd2fcd11fefa9ddc98e3a4f78a18e0a
Garoe <garoedp@gmail.com>
2016-03-18 17:55:06 +0000

    heat-map, removed unused functions

--- heat_map/error_fnc/histogramErrorOpti.m
+++ heat_map/error_fnc/histogramErrorOpti.m
@@ -1,41 +1,56 @@
-function [ cerror ] = histogramErrorOpti( goal_im, imga, goal_mask, img_mask )
-%HISTOGRAMERROROPTI Computes an error measure between two images
-%   CERROR = HISTOGRAMERROROPTI(IMGA, GOAL_IM) this is an optimized
+function [ cerror ] = histogramErrorOpti( goal_imgs, test_imgs, goal_mask, ...
+    img_mask)
+%HISTOGRAMERROROPTI Compues an error measure between several images
+%   CERROR = HISTOGRAMERROROPTI(GOAL_IMGS, TEST_IMGS) this is an optimized
 %   version of HISTOGRAM_ERROR, assumes RGB images, ignores black pixels
 %   and if the goal image changes, call clear 'histogramErrorOpti';
+%   GOAL_IMGS and TEST_IMGS are same sized cells with the images to compare
 
-%TODO Add goal_mask argument
-persistent HC_GOAL IMGA_FACTOR
+persistent HC_GOAL TESTIM_FACTOR
 
 % Create 256 bins, image can be 0..255
 edges = linspace(0, 255, 256);
 
 if isempty(HC_GOAL)
-    sub_img = goal_im(:, :, 1);
-    HC_GOAL(1, :) = histcounts( sub_img(goal_mask), edges);
-    sub_img = goal_im(:, :, 2);
-    HC_GOAL(2, :) = histcounts( sub_img(goal_mask), edges);
-    sub_img = goal_im(:, :, 3);
-    HC_GOAL(3, :) = histcounts( sub_img(goal_mask), edges);
+    HC_GOAL = cell(numel(goal_imgs), 1);
+    TESTIM_FACTOR = zeros(1, numel(goal_imgs));
     
-    % Normalize by the number of pixels
-    HC_GOAL = HC_GOAL ./ sum(goal_mask(:) == 1);
-    
-    IMGA_FACTOR = 1 / sum(img_mask(:) == 1);
+    for i=1:numel(goal_imgs)
+        sub_img = goal_imgs{i}(:, :, 1);
+        HC_GOAL{i}(1, :) = histcounts( sub_img(goal_mask{i}), edges);
+        sub_img = goal_imgs{i}(:, :, 2);
+        HC_GOAL{i}(2, :) = histcounts( sub_img(goal_mask{i}), edges);
+        sub_img = goal_imgs{i}(:, :, 3);
+        HC_GOAL{i}(3, :) = histcounts( sub_img(goal_mask{i}), edges);
+        
+        % Normalize by the number of pixels
+        HC_GOAL{i} = HC_GOAL{i} ./ sum(goal_mask{i}(:) == 1);
+        
+        TESTIM_FACTOR(i) = 1 / sum(img_mask{i}(:) == 1);
+    end
 end
 
-% Compute the histogram count for each color channel
-subImga = imga(:, :, 1);
-Na(1, :) = histcounts( subImga(img_mask), edges) * IMGA_FACTOR;
-
-subImga = imga(:, :, 2);
-Na(2, :) = histcounts( subImga(img_mask), edges) * IMGA_FACTOR;
-
-subImga = imga(:, :, 3);
-Na(3, :) = histcounts( subImga(img_mask), edges) * IMGA_FACTOR;
-
 % Compute the error as in Dobashi et. al. 2012
-cerror = (sum(abs(Na(1, :) - HC_GOAL(1, :))) + sum(abs(Na(2, :) - ...
-    HC_GOAL(2, :))) + sum(abs(Na(3, :) - HC_GOAL(3, :)))) / 6;
+cerror = 0;
+for i=1:numel(goal_imgs)
+    
+    % Compute the histogram count for each color channel
+    subImga = test_imgs{i}(:, :, 1);
+    hc_test(1, :) = histcounts(subImga(img_mask{i}), edges) * TESTIM_FACTOR(i);
+    
+    subImga = test_imgs{i}(:, :, 2);
+    hc_test(2, :) = histcounts(subImga(img_mask{i}), edges) * TESTIM_FACTOR(i);
+    
+    subImga = test_imgs{i}(:, :, 3);
+    hc_test(3, :) = histcounts(subImga(img_mask{i}), edges) * TESTIM_FACTOR(i);
+    
+    cerror = cerror + (sum(abs(hc_test(1, :) - HC_GOAL{i}(1, :))) + ...
+        sum(abs(hc_test(2, :) - HC_GOAL{i}(2, :))) + ...
+        sum(abs(hc_test(3, :) - HC_GOAL{i}(3, :)))) / 6;
+end
+
+% Divide by the number of images so that the error function is still in the
+% range of 0..1
+cerror = cerror ./ numel(goal_imgs);
 
 end
--- heat_map/fitness_fnc/heat_map_fitness.m
+++ heat_map/fitness_fnc/heat_map_fitness.m
@@ -1,11 +1,11 @@
 function [ error ] = heat_map_fitness( heat_map_v, xyz, whd, error_foo, ...
     scene_name, scene_img_folder, output_img_folder_name, sendMayaScript, ...
     port, mrLogPath, goal_img, goal_mask, img_mask)
-%HEAT_MAP_FITNESSN Heat map fitness function
-%   ERROR = HEAT_MAP_FITNESS( HEAT_MAP_V, XYZ, WHD, ERROR_FOO, ...
-%   SCENE_NAME, SCENE_IMG_FOLDER, OUTPUT_IMG_FOLDER_NAME, SENDMAYASCRIPT, ...
-%   PORT, MRLOGPATH, GOAL_IMG) Fitness function for optimization
-%   algorithms
+%HEAT_MAP_FITNESS Heat map fitness function
+%    Like heat_map_fitness function but it supports several goal images
+%    given in a cell
+%
+%    See also HEAT_MAP_FITNESS
 
 persistent CACHE
 
@@ -41,46 +41,54 @@ for pop=1:size(heat_map_v, 1)
         cmd = [cmd '$HOME/' heat_map_path(3:end) '\"'];
         sendToMaya(sendMayaScript, port, cmd);
         
-        %% Set the folder and name of the render image
-        cmd = 'setAttr -type \"string\" defaultRenderGlobals.imageFilePrefix \"';
-        cmd = [cmd scene_name '/' output_img_folder_name tmpdirName '/fireimage' '\"'];
-        sendToMaya(sendMayaScript, port, cmd);
+        num_goal = numel(goal_img);
+        c_img = cell(num_goal, 1);
         
-        %% Render the image
-        % This command only works on Maya running in batch mode, if running with
-        % the GUI, use Mayatomr -preview. and then save the image with
-        % $filename = "Path to save";
-        % renderWindowSaveImageCallback "renderView" $filename "image";
-        startTime = tic;
-        cmd = 'Mayatomr -verbosity 2 -render -renderVerbosity 2';
-        sendToMaya(sendMayaScript, port, cmd, 1, mrLogPath);
-        %fprintf('Image rendered with');
-        
-        %% Compute the error with respect to the goal image
-        try
-            c_img = imread([output_img_folder tmpdirName '/fireimage.tif']);
-        catch ME
-            msg = ['Could not read rendered image, make sure only one camera' ...
-                ' is renderable in the Maya scene'];
-            causeException = MException('MATLAB:heat_map_fitness',msg);
-            ME = addCause(ME,causeException);
-            rethrow(ME);
+        for i=1:num_goal
+            istr = num2str(i);
+            
+            %% Activate the current camera
+            cmd = ['setAttr \"camera' istr 'Shape.renderable\" 1'];
+            sendToMaya(sendMayaScript, port, cmd);
+            
+            %% Set the folder and name of the render image
+            cmd = 'setAttr -type \"string\" defaultRenderGlobals.imageFilePrefix \"';
+            cmd = [cmd scene_name '/' output_img_folder_name tmpdirName '/fireimage' ...
+                istr '\"'];
+            sendToMaya(sendMayaScript, port, cmd);
+            
+            %% Render the image
+            cmd = 'Mayatomr -verbosity 2 -render -renderVerbosity 2';
+            sendToMaya(sendMayaScript, port, cmd, 1, mrLogPath);
+            %fprintf('Image rendered with');
+            
+            %% Deactivate the current camera
+            cmd = ['setAttr \"camera' istr 'Shape.renderable\" 0'];
+            sendToMaya(sendMayaScript, port, cmd);
+            
+            %% Compute the error with respect to the goal image
+            try
+                c_img{i} = imread([output_img_folder tmpdirName '/fireimage' istr '.tif']);
+            catch ME
+                msg = 'Could not read rendered image, try disabling any extra camera';
+                causeException = MException('MATLAB:heat_map_fitness',msg);
+                ME = addCause(ME,causeException);
+                rethrow(ME);
+            end
+            
+            c_img{i} = c_img{i}(:,:,1:3); % Transparency is not used, so ignore it
         end
-        c_img = c_img(:,:,1:3); % Transparency is not used, so ignore it
         
         % Evaluate all the error functions, usually only one will be given
         for i=1:num_error_foos
-            if(sum(c_img(:)) == 0)
-                % If the rendered image is completely black set the error manually
+            if(any(cellfun(@(x)sum(x(:)), c_img) == 0))
+                % If any of the rendered image is completely black set the error manually
                 error(i, pop) = realmax;
             else
                 error(i, pop) = sum(feval(error_foo{i}, goal_img, c_img, ...
                     goal_mask, img_mask));
             end
         end
-        
-        % Print the rest of the information on the same line
-        %fprintf(' error %.2f, in %.2f seconds.\n', error(1), toc(startTime));
         
         % Delete the temporary files
         system(['rm -rf ' tmpdir ' < /dev/null &']);
