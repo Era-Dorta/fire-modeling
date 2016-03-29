42d6b28cfba795b3924ded39e4a89d288f49e347
Garoe <garoedp@gmail.com>
2016-03-29 15:24:52 +0100

    multiplegoals, updated run script for fire attr search
    The new scripts supports multiple goal images and includes arguments for
    the mask images

--- heat_map/runFireAttrSearch2.sh
+++ heat_map/runFireAttrSearch2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "$#" -lt 3 ]; then
-	if [ "$#" -eq 2 ]; then
+if [ "$#" -lt 5 ]; then
+	if [ "$#" -eq 4 ]; then
 		# If no arguments are given set number of Maya instances to number of 
 		# cores divided by three
 		NUM_MAYA=$(grep -c ^processor /proc/cpuinfo)
@@ -11,14 +11,17 @@ if [ "$#" -lt 3 ]; then
 		echo ""
 		echo "Not enough input arguments"
 		echo ""
-		echo "Usage: runFireAttrSearch2.sh <solver> <goal-image> <maya_threads>"
+		echo "Usage: runFireAttrSearch2.sh <solver> <goal-image> <goal-mask> "
+		echo "            <synthetic-mask> <maya_threads>"
 		echo ""
 		echo "	Where <solver> can be any of:"
 		echo "	\"ga\"    -> Genetic Algorithm"
 		echo "	\"sa\"    -> Simulated Annealing"
 		echo "	\"grad\"  -> Gradient Descent"
 		echo ""
-		echo "	<goal-image> path to a goal image"
+		echo "	<goal-image> path to goal image/s separated by ;"
+		echo "	<goal-mask> path to goal mask image/s separated by ;"
+		echo "	<synthetic-mask> path to synthetic mask image/s separated by ;"
 		echo ""
 		echo "	<maya_threads> must be an positive integer which indicates how many"
 		echo "	Maya instances will launched for rendering"
@@ -27,21 +30,42 @@ if [ "$#" -lt 3 ]; then
 		exit 0 
 	fi
 else
-	NUM_MAYA=$3
+	NUM_MAYA=$5
 fi
 
-# Put the solver in a decent variable name
-SOLVER="${1}"
+# Put the solver in a variable name and surround with ''
+SOLVER="'${1}'"
 
-# Get the full path to the image
-GOAL_IMG=$(readlink -e "${2}")
+# Get the full paths of the images and surround with '' and {} for matlab
+IFS=';' read -ra AUX <<< "${2}"
+GOAL_IMG=""
+for i in "${AUX[@]}"; do
+    GOAL_IMG="${GOAL_IMG},'$(readlink -e "${i}")'"
+done
+GOAL_IMG="{${GOAL_IMG:1}}"
+
+IFS=';' read -ra AUX <<< "${3}"
+GOAL_MASK=""
+for i in "${AUX[@]}"; do
+    GOAL_MASK="${GOAL_MASK},'$(readlink -e "${i}")'"
+done
+GOAL_MASK="{${GOAL_MASK:1}}"
+
+IFS=';' read -ra AUX <<< "${4}"
+SYNT_MASK=""
+for i in "${AUX[@]}"; do
+    SYNT_MASK="${SYNT_MASK},'$(readlink -e "${i}")'"
+done
+SYNT_MASK="{${SYNT_MASK:1}}"
 
 # Each Maya will listen to this port + (current Maya number - 1)
 INIT_PORT="2222"
 
 # Create random name for the log file to avoid clashes with other matlab logs
 LOGFILE=`mktemp matlabXXXXXXXXXXXXXXXXXXXXX.log`
-LOGFILE=`pwd`"/"$LOGFILE
+
+# Get full path and add '' for matlab notation
+LOGFILE="'`pwd`"/"$LOGFILE'"
 
 # Get this script path
 CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -85,8 +109,9 @@ done
 
 PORTS="[${PORTS}]"
 
-# Runs matlab in batch mode with low priority
-nice -n20 matlab -nodesktop -nosplash -r "fire_attr_search2('${SOLVER}', '${GOAL_IMG}', $PORTS, '$LOGFILE')" -logfile $LOGFILE
+# Runs matlab in batch mode with low priority    
+nice -n20 matlab -nodesktop -nosplash -r "fire_attr_search2(${SOLVER}, ${GOAL_IMG}, \
+   ${GOAL_MASK}, ${SYNT_MASK}, $PORTS, $LOGFILE)" -logfile $LOGFILE
 
 # Close all the Maya instances
 for i in `seq 1 $NUM_MAYA`;
