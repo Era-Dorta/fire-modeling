164c9c84167352bc0bb0d39d302070a19b11b3e7
Garoe <garoedp@gmail.com>
2016-05-02 15:01:22 +0100

    Minor fix in custom move file function

--- heat_map/file_management/move_file.m
+++ heat_map/file_management/move_file.m
@@ -1,7 +1,7 @@
 function move_file( origin, destiny )
 % Tries to move a file from origin to destiny
 if(exist(origin, 'file'))
-    if(movefile(origin,destiny) == 0)
+    if(movefile(origin,destiny) == 1)
         disp([origin ' saved in ' destiny]);
     else
         disp([origin ' could not be moved from current location']);
