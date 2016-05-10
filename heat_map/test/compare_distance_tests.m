d569ef5b97088f0b5384b724b69d5fbdb4257675
Garoe <garoedp@gmail.com>
2016-05-10 17:48:56 +0100

    compare_distance_tests, added support for comparisons with population

--- heat_map/test/compare_distance_tests.m
+++ heat_map/test/compare_distance_tests.m
@@ -23,8 +23,15 @@ hm_data_folders(to_del_idx) = [];
 
 % Initialize variables
 num_hm = numel(hm_data_folders);
-dist_foo_str = cell(1, num_hm);
-mse_error = zeros(1, num_hm);
+dist_foo_map = containers.Map();
+pop_map = containers.Map();
+
+% Keys in order of appeareance
+dist_foo_map_keys = {};
+pop_map_keys = {};
+
+% Rows for each distance sample, columns for each distance type
+mse_error = [];
 
 % Read the optimized images for each solver run
 for i=1:num_hm
@@ -33,11 +40,29 @@ for i=1:num_hm
     
     % Load the data file
     opts = load(fullfile(hm_data_folder, 'summary_file.mat'));
+    opts_solver = opts.options.options;
     opts = opts.summary_data;
     num_goal = numel(opts.goal_img_path);
     
-    % Get the distance function name
-    dist_foo_str{i} = func2str(opts.dist_foo);
+    % Get the index for the distance function name
+    dist_foo_name = func2str(opts.dist_foo);
+    if isKey(dist_foo_map, dist_foo_name)
+        dist_idx = dist_foo_map(dist_foo_name);
+    else
+        dist_idx = dist_foo_map.Count + 1;
+        dist_foo_map(dist_foo_name) = dist_idx;
+        dist_foo_map_keys{end + 1} = dist_foo_name;
+    end
+    
+    % Get the index for the population size
+    popSizeStr = num2str(opts_solver.PopulationSize);
+    if isKey(pop_map, popSizeStr)
+        pop_idx = pop_map(popSizeStr);
+    else
+        pop_idx = pop_map.Count + 1;
+        pop_map(popSizeStr) = pop_idx;
+        pop_map_keys{end + 1} = popSizeStr;
+    end
     
     % Read all the optimized images
     cam_num = 1;
@@ -77,23 +102,36 @@ for i=1:num_hm
         img_mask{j} = true(size(img{j}, 1), size(img{j}, 2));
     end
     
-    mse_error(i) = MSE(goal_img, img, goal_mask, img_mask);
+    mse_error(pop_idx, dist_idx) = MSE(goal_img, img, goal_mask, img_mask);
 end
 
 %% Plot the comparison
+% Do not use the .keys in plotting as they are not output in order
 % Scape the underscores to avoid substript in the legend
-dist_foo_str1 = strrep(dist_foo_str, '_', '\_');
+dist_foo_map_keys_u = strrep(dist_foo_map_keys, '_', '\_');
 
 fig_h = figure;
 hold on;
 
-% Plot each one separately to be able to use legend
-colors = {'b' , 'r' , 'g', 'c' , 'm' , 'y' , 'k' , 'w'};
-for i=1:numel(mse_error)
-    bar(i, mse_error(i), colors{i});
+if size(mse_error, 1) == 1
+    % Plot each one separately to be able to use legend and colors
+    colors = {'b' , 'r' , 'g', 'c' , 'm' , 'y' , 'k' , 'w'};
+    for i=1:numel(mse_error)
+        bar(i, mse_error(i), colors{i});
+    end
+else
+    bar(mse_error);
 end
+
+xlabel('Population');
+ylabel('MSE Error');
+
+% X-axis use the population number
+set(gca,'XTick', 1:size(mse_error, 2));
+set(gca,'xticklabel', pop_map_keys);
+
 set(gca,'ylim', [0, 1]); % Maximum error is 1
-legend(dist_foo_str1);
+legend(dist_foo_map_keys_u);
 hold off;
 
 %% Save the output
@@ -112,14 +150,22 @@ else
 end
 
 % Put the data together in a cell string
-data_str = cell(2, numel(mse_error));
-for i=1:numel(mse_error)
-    data_str{1, i} = dist_foo_str{i};
-    data_str{2, i} = num2str(mse_error(i), '%e');
+data_str = cell(3, dist_foo_map.Count * pop_map.Count);
+k = 1;
+for i=1:pop_map.Count
+    for j=1:dist_foo_map.Count
+        data_str{1, k} = dist_foo_map_keys{j};
+        data_str{2, k} = pop_map_keys{i};
+        
+        imse = pop_map(pop_map_keys{i});
+        jmse = dist_foo_map(dist_foo_map_keys{j});
+        data_str{3, k} = num2str(mse_error(imse,jmse), '%e');
+        k = k + 1;
+    end
 end
 
-fprintf(fileId, 'Distance comparison\nError function, MSE error\n');
-fprintf(fileId, '%s, %s\n', data_str{:});
+fprintf(fileId, 'Distance comparison\nError function, Population, MSE error\n');
+fprintf(fileId, '%s, %s, %s\n', data_str{:});
 
 % Save the figure
 figurePath = fullfile(data_folder, 'mse-comparison');
