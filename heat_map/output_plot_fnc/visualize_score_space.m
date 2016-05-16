b982300e11da9fc034c0e4dd4607ca200aeffe31
Garoe <garoedp@gmail.com>
2016-05-16 09:33:01 +0100

    GA, workaround for legend bug in Matlab in visualize_score_space

--- heat_map/output_plot_fnc/visualize_score_space.m
+++ heat_map/output_plot_fnc/visualize_score_space.m
@@ -19,6 +19,11 @@ else
     fig_h = figure('Position', [1316 28 570 422]);
 end
 
+% First scatter text in legend appears as blue do to bug in Matlab 2015b,
+% do a fake plot with the same color, save the handle and hide the plot
+hl(1) = plot(1, 1, 'gx');
+hl(1).Visible = 'off';
+
 hold on;
 
 % Plot initial population
@@ -29,23 +34,24 @@ scatter(Y(1: current_size + offset, 1), Y(1: current_size + offset,2), 'gx');
 % Plot final population
 offset = offset + current_size;
 current_size = size(L.FinalPopulation, 1);
-scatter(Y(offset+1: current_size+offset, 1), Y(offset+1:current_size+offset,2), 'bx');
+hl(2) = scatter(Y(offset+1: current_size+offset, 1), Y(offset+1:current_size+offset,2), 'bx');
 
 % Plot the best per generation
 offset = offset + current_size;
 current_size = size(L.BestPopGen, 1);
 
 % Start with a star
-plot(Y(offset+1, 1), Y(offset+1,2), '*r');
+hl(3) = plot(Y(offset+1, 1), Y(offset+1,2), '*r');
+
+% Line from start to second
+plot(Y(offset+1:offset+2, 1), Y(offset+1:offset+2,2), '--r');
 
 % Line with markers from second to final
-plot(Y(offset+2:current_size+offset, 1), Y(offset+2:current_size+offset,2), '--or');
+hl(4) = plot(Y(offset+2:current_size+offset, 1), Y(offset+2:current_size+offset,2), '--or');
 
-legend('Initial Population', 'Final Population', ...
+% Legend using only the handlers that we are interested in
+legend(hl, 'Initial Population', 'Final Population', ...
     'Optimization Path Start Point', 'Optimization Path');
-
-% Line from start to next
-plot(Y(offset+1:offset+2, 1), Y(offset+1:offset+2,2), '--r');
 
 hold off;
 
