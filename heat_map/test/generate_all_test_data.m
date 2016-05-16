183c03ddebe20d31fe51e18b0dea8ee44778d82a
Garoe <garoedp@gmail.com>
2016-05-16 16:42:39 +0100

    Heatmap, added helper function to execute all args_test functions

---
+++ heat_map/test/generate_all_test_data.m
@@ -0,0 +1,25 @@
+function generate_all_test_data()
+%GENERATE_ALL_TEST_DATA Generates args_test.mat files
+%   GENERATE_ALL_TEST_DATA fills the heat_map/test/data folder by executing
+%   all the heat_map/test/functions/args_test<digit> functions
+
+current_dir = fileparts(mfilename('fullpath'));
+args_files_folders = dir(fullfile(current_dir, 'functions'));
+args_files_folders = {args_files_folders.name};
+
+% Remove anything that is not args_test[digit]
+to_del_idx = [];
+for i=1:numel(args_files_folders)
+    if isempty(regexp(args_files_folders{i},'args_test[0-9]+', 'ONCE'))
+        to_del_idx = [to_del_idx; i];
+    end
+end
+args_files_folders(to_del_idx) = [];
+
+for i=1:numel(args_files_folders)
+    [~, foo_name] = fileparts(args_files_folders{i});
+    feval(foo_name);
+end
+
+end
+
