df812db88c8d4a70775052f89e0eeade1e1cf9d9
Garoe <garoedp@gmail.com>
2016-05-17 10:46:34 +0100

    Optimized color conversion max range function

--- heat_map/utilities/colorspace/color_space_max_range.m
+++ heat_map/utilities/colorspace/color_space_max_range.m
@@ -9,36 +9,33 @@ function [out_file_path] = color_space_max_range()
 out_file_path = fullfile(fileparts(mfilename('fullpath')), 'colors_range.mat');
 
 if(exist(out_file_path, 'file'))
-    error(['File ' out_file_path ' exits, overwrite not allowed']);
+    % error(['File ' out_file_path ' exits, overwrite not allowed']);
 end
 
 spaces = {'YPbPr', 'YCbCr', 'JPEG-YCbCr', 'YDbDr', 'YIQ','YUV', ...
     'HSV', 'HSL', 'HSI', 'XYZ', 'Lab', 'Luv', 'LCH', 'CAT02 LMS'};
 
-in_img = zeros(1,1,3);
 max_color = cell(size(spaces));
-max_color(:) = {zeros(1,1,3)};
+max_color(:) = {zeros(1,3)};
 
 min_color = cell(size(spaces));
-min_color(:) = {zeros(1,1,3) + Inf};
+min_color(:) = {zeros(1,3) + Inf};
+
+% All combinations of RGB values from 0 to 255.
+num_combinations = 256^3;
+in_img = permn(0:255, 3);
+%Reshape to image format
+in_img = double(reshape(in_img, num_combinations, 1, 3));
 
 for i=1:numel(spaces)
-    for j=0:255
-        for k=0:255
-            for l=0:255
-                in_img(:,:,1) = j; in_img(:,:,2) = k; in_img(:,:,3) = l;
-                
-                out_color = colorspace([spaces{i},'<-RGB'], double(in_img));
-                
-                max_color{i} = max(max_color{i}, out_color);
-                min_color{i} = min(min_color{i}, out_color);
-            end
-        end
-    end
+    out_color = colorspace([spaces{i},'<-RGB'], in_img);
+    out_color = reshape(out_color, num_combinations, 3);
+    
+    max_color{i} = max(out_color);
+    min_color{i} = min(out_color);
 end
 
-
-save(out_file_path, 'max_color', 'min_color');
+save(out_file_path, 'max_color', 'min_color', 'spaces');
 
 end
 
