1577d36f4b0105439bbee3f24f821c884c7c0a83
Garoe <garoedp@gmail.com>
2016-05-23 15:22:56 +0100

    Added new test file

---
+++ heat_map/test/functions/args_test22.m
@@ -0,0 +1,26 @@
+function [args_path] = args_test22()
+%ARGS_TEST22 Candle synthetic32x32x32
+%   ARGS_PATH = ARGS_TEST22() Returns in ARGS_PATH the file path of a .mat
+%   file with arguments defined here.
+%
+%   See also heatMapReconstruction, args_test_template,
+%   args_test_solver_template
+
+%% Change common parameters
+data_dir = fullfile(fileparts(fileparts(mfilename('fullpath'))), 'data');
+args_path = fullfile(data_dir, [mfilename('clas') '.mat']);
+args_test_template(args_path);
+
+L = load(args_path);
+
+goal_img_path = {'~/maya/projects/fire/data/fire-test-pics/pegoraro1.png'};
+goal_mask_img_path = {'~/maya/projects/fire/data/fire-test-pics/trimap/pegoraro1-mask.png'};
+raw_file_path = '~/maya/projects/fire/data/from_dmitry/NewData/oneFlame/synthetic32x32x32.raw';
+
+scene_name = 'test99_synthetic32x32x32';
+scene_img_folder = fullfile(L.project_path, 'images', [scene_name '/']);
+
+% Save all but L
+save(args_path, '-regexp','^(?!(L|data_dir)$).', '-append');
+
+end
