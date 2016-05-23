06cbf2e73f3ad6a852eac413d4e287e3e4da74bc
Garoe <garoedp@gmail.com>
2016-05-23 14:26:16 +0100

    Minor fix in test script

--- heat_map/test/scripts/runCreationHeuristic3BinNumber.sh
+++ heat_map/test/scripts/runCreationHeuristic3BinNumber.sh
@@ -12,7 +12,7 @@ nice -n20 matlab -nodesktop -nosplash -r "args_test21(); exit();"
 BIN_SIZES=(10 50 100 256)
 
 for i in ${BIN_SIZES[@]}; do
-	"args_path = '~/bath-fire-shader/heat_map/test/data/args_test21.mat'; creation_fnc_n_bins = ${i}; save(args_path, 'creation_fnc_n_bins', '-append'); exit();"
+	nice -n20 matlab -nodesktop -nosplash -r "args_path = '~/bath-fire-shader/heat_map/test/data/args_test21.mat'; creation_fnc_n_bins = ${i}; save(args_path, 'creation_fnc_n_bins', '-append'); exit();"
 	
 	#Optimize using the parameters defined in the previous data files
 	"$CDIR/runHeatMapReconstruction.sh" ~/bath-fire-shader/heat_map/test/data/args_test21.mat
