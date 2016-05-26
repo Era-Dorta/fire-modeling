98b50c7c30b3210eb5dec04ab32460aa33a605a2
Garoe <garoedp@gmail.com>
2016-05-26 15:57:03 +0100

    Minor fix in getImgcombinedHistogram

--- heat_map/creation_fnc/utilities/getImgCombinedHistogram.m
+++ heat_map/creation_fnc/utilities/getImgCombinedHistogram.m
@@ -9,7 +9,7 @@ function [ hc_goal ] = getImgCombinedHistogram( img, img_mask, n_bins, ...
 %   See also getColorFromHistoIndex, getImgMeanColor, getImgStdColor
 
 %% Compute histogram of the goal image
-if nargin == 3
+if nargin == 4
     do_normalize = false;
 end
 
