4f23b1768617fa7b4674b87b92e0cdf19633578e
Garoe <garoedp@gmail.com>
2016-05-31 17:39:38 +0100

    Added args_test for big_flame2 data

---
+++ heat_map/test/functions/args_test26.m
@@ -0,0 +1,27 @@
+function [args_path] = args_test26()
+%ARGS_TEST26 Big flame2, Pegoraro1 Goal
+%   ARGS_PATH = ARGS_TEST26() Returns in ARGS_PATH the file path of a .mat
+%   file with arguments defined here.
+%
+%   See also heatMapReconstruction, args_test_template,
+%   args_test_solver_template
+
+%% Change common parameters
+data_dir = fullfile(fileparts(fileparts(mfilename('fullpath'))), 'data');
+args_path = fullfile(data_dir, [mfilename('clas') '.mat']);
+args_test_template(args_path);
+
+L = load(args_path);
+
+goal_img_path = {'~/maya/projects/fire/data/fire-test-pics/pegoraro1.png'};
+goal_mask_img_path = {'~/maya/projects/fire/data/fire-test-pics/trimap/pegoraro1-mask.png'};
+mask_img_path = {'~/maya/projects/fire/images/test100_bigflame2/mask-bigflame2.png'};
+raw_file_path = 'data/from_dmitry/bigFlame2/synthetic00000.raw';
+
+scene_name = 'test100_bigflame2';
+scene_img_folder = fullfile(L.project_path, 'images', [scene_name '/']);
+
+% Save all but L
+save(args_path, '-regexp','^(?!(L|data_dir)$).', '-append');
+
+end
