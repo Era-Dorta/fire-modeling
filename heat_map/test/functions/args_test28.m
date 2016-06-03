6d87b6ed5074d0ce8291d2eb4a9ab3410fcc075b
Garoe <garoedp@gmail.com>
2016-06-03 12:03:52 +0100

    Added test functions for Maya data

---
+++ heat_map/test/functions/args_test28.m
@@ -0,0 +1,27 @@
+function [args_path] = args_test28()
+%ARGS_TEST28 Maya-Data1, Pegoraro1 Goal
+%   ARGS_PATH = ARGS_TEST28() Returns in ARGS_PATH the file path of a .mat
+%   file with arguments defined here.
+%
+%   See also heatMapReconstruction, args_test_template,
+%   args_test_solver_template
+
+%% Change common parameters
+data_dir = fullfile(fileparts(fileparts(mfilename('fullpath'))), 'data');
+args_path = fullfile(data_dir, [mfilename('clas') '.mat']);
+args_test_template(args_path);
+
+L = load(args_path);
+
+goal_img_path = {'~/maya/projects/fire/data/fire-test-pics/pegoraro1.png'};
+goal_mask_img_path = {'~/maya/projects/fire/data/fire-test-pics/trimap/pegoraro1-mask.png'};
+mask_img_path = {'~/maya/projects/fire/images/test102_maya_data/flame-30-mask1.png'};
+raw_file_path = 'data/heat_maps/maya-flame-preset/density30.raw';
+
+scene_name = 'test102_maya_data';
+scene_img_folder = fullfile(L.project_path, 'images', [scene_name '/']);
+
+% Save all but L
+save(args_path, '-regexp','^(?!(L|data_dir)$).', '-append');
+
+end
