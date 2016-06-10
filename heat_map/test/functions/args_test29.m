1fb52a97d2f15d7b271c237791fce01182f7ae98
Garoe <garoedp@gmail.com>
2016-06-10 10:33:12 +0100

    Using new raw_files in test28 and 29

--- heat_map/test/functions/args_test29.m
+++ heat_map/test/functions/args_test29.m
@@ -16,7 +16,7 @@ L = load(args_path);
 goal_img_path = {'~/maya/projects/fire/data/fire-test-pics/pegoraro2.png'};
 goal_mask_img_path = {'~/maya/projects/fire/data/fire-test-pics/trimap/pegoraro2-mask.png'};
 mask_img_path = {'~/maya/projects/fire/images/test102_maya_data/flame-30-mask1.png'};
-raw_file_path = 'data/heat_maps/maya-flame-preset/density30.raw';
+raw_file_path = 'data/heat_maps/maya-flame-preset/temperature30-reduced.raw';
 
 scene_name = 'test102_maya_data';
 scene_img_folder = fullfile(L.project_path, 'images', [scene_name '/']);
