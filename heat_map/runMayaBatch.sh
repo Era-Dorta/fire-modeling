4e3585e38eaf63c5f8ea98b76cf47a7a0bdb431c
Garoe <garoedp@gmail.com>
2016-06-14 10:15:31 +0100

    Changed spaces for tabs in runMayaBath.sh

--- heat_map/runMayaBatch.sh
+++ heat_map/runMayaBatch.sh
@@ -9,50 +9,50 @@ function isPortOpen()
 
 function runSingleMaya()
 {
-    local PORT=$1
+	local PORT=$1
 
-    isPortOpen "$PORT"
+	isPortOpen "$PORT"
 
-    # If it fails, tell the user to use a different port
-    if [ "$?" -eq 0 ] ; then
-	    return 0
-    fi
+	# If it fails, tell the user to use a different port
+	if [ "$?" -eq 0 ] ; then
+		return 0
+	fi
 
 	# Run with setsid to protect Maya from the parent ctrl-c, allows Maya to be 
 	# closed gracefully
-    setsid nice -n20 oriMaya -batch -command "commandPort -n \":$PORT\";" &> /dev/null &
-    
-    # sendToMaya command will wait for Maya to open and be ready, but weird
-    # errors sometime happen, so give Maya some extra time before even
-    # trying to open the port
-    sleep 1
-    
-    return 1
+	setsid nice -n20 maya -batch -command "commandPort -n \":$PORT\";" &> /dev/null &
+
+	# sendToMaya command will wait for Maya to open and be ready, but weird
+	# errors sometime happen, so give Maya some extra time before even
+	# trying to open the port
+	sleep 1
+
+	return 1
 }
 
 function runSingleMayaLog()
 {
-    local PORT=$1
+	local PORT=$1
 
-    isPortOpen "$PORT"
+	isPortOpen "$PORT"
 
-    # If it fails, tell the user to use a different port
-    if [ "$?" -eq 0 ] ; then
-	    return 0
-    fi
-    
-    local LOGNAME="mayaLog-${PORT}.log"
-    > "${LOGNAME}" # Create/Truncate the file
+	# If it fails, tell the user to use a different port
+	if [ "$?" -eq 0 ] ; then
+		return 0
+	fi
+
+	local LOGNAME="mayaLog-${PORT}.log"	
+	> "${LOGNAME}" # Create/Truncate the file
 
 	# Redirect with >> so the file can be truncated by each Matlab run
-    setsid nice -n20 oriMaya -batch -command "commandPort -n \":$PORT\";" &>> "${LOGNAME}" &
-    
-    # sendToMaya command will wait for Maya to open and be ready, but weird
-    # errors sometime happen, so give Maya some extra time before even
-    # trying to open the port
-    sleep 1
-    
-    return 1
+	setsid nice -n20 maya -batch -command "commandPort -n \":$PORT\";" &>> "${LOGNAME}" &
+
+	# sendToMaya command will wait for Maya to open and be ready, but weird
+	# errors sometime happen, so give Maya some extra time before even
+	# trying to open the port
+	sleep 1
+
+	return 1
 }
 
 PORT=2222
@@ -65,34 +65,34 @@ CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ "$#" -ge 1 ]; then
 	PORT=$1
 	if [ "$#" -ge 2 ]; then
-	    NUM_MAYA=$2
-	    if [ "$#" -ge 3 ]; then
-	    	USE_LOG=$3
-    	fi
-    fi
+		NUM_MAYA=$2
+		if [ "$#" -ge 3 ]; then
+			USE_LOG=$3
+		fi
+	fi
 fi
 
 for i in `seq 1 $NUM_MAYA`;
 do
-    NEW_PORT=$((${PORT} + $i - 1))
-    
-    if [ ${USE_LOG} -eq 0 ]; then
-    	runSingleMaya $NEW_PORT
-    else
-    	runSingleMayaLog $NEW_PORT
-    fi
+	NEW_PORT=$((${PORT} + $i - 1))
 
-    if [ "$?" -eq 1 ]; then
-        echo "Launched Maya:${NEW_PORT}"
+	if [ ${USE_LOG} -eq 0 ]; then
+		runSingleMaya $NEW_PORT
+	else
+		runSingleMayaLog $NEW_PORT
+	fi
+
+	if [ "$?" -eq 1 ]; then
+		echo "Launched Maya:${NEW_PORT}"
 	else
 		echo "Could not launch Maya:${NEW_PORT}"
-	
+
 		# Close all the previous Maya instances	
 		if [ "$i" -gt 1 ]; then
 			"$CDIR/closeMayaBatch.sh" "$PORT" "$(($i - 1))"
 		fi
-		
+
 		exit 2
-    fi
+	fi
 done
 
