ff9ee9822b7b4530293581fc775aae816af266b7
Garoe <garoedp@gmail.com>
2016-06-22 11:42:39 +0100

    Using gaussian filter instead of disk filter in gaussian blur function

--- heat_map/utilities/apply_gaussian_blur.m
+++ heat_map/utilities/apply_gaussian_blur.m
@@ -1,4 +1,4 @@
-function [ out_hm ] = apply_gaussian_blur( in_hm, bg, g_size )
+function [ out_hm ] = apply_gaussian_blur( in_hm, bg, g_size, g_sigma )
 %APPLY_GAUSSIAN_BLUR Apply Gaussia blur to heatmap
 %   [ OUT_HM ] = APPLY_GAUSSIAN_BLUR( IN_HM ) Blur the heatmap
 %   IN_HM and get the result in OUT_HM.
@@ -8,24 +8,40 @@ function [ out_hm ] = apply_gaussian_blur( in_hm, bg, g_size )
 %   is 0.
 %
 %   [ OUT_HM ] = APPLY_GAUSSIAN_BLUR( IN_HM, BG, G_SIZE ) The radius of
-%   the gaussian filter can be set with G_SIZE, note that actual size is
-%   2 * G_SIZE + 1. Default is 1.
+%   the gaussian filter can be set with G_SIZE. Default is 3.
+%
+%   [ OUT_HM ] = APPLY_GAUSSIAN_BLUR( IN_HM, BG, G_SIZE, G_SIGMA ) The
+%   amount of blurring can be controled with G_SIGMA. Default is 0.5, which
+%   approximately takes 50% from the original voxel value and 50% from its 
+%   neighbours.
 
 if nargin == 1
-    % BAckground values
+    % Background values
     bg = 0;
 end
 
 if nargin <= 2
-    % g_size of 1 generates a 3x3 filter
-    g_size = 1;
+    % g_size of 3 generates a 3x3 filter
+    g_size = 3;
 end
 
-h = fspecial('disk', g_size);
+if nargin <= 3
+    % g_sigma of 0.5, give the centre voxel a weight of 0.4874
+    g_sigma = 0.5;
+end
 
-% Replicate the filter so it is three-dimensional, divide by three so that
+h = fspecial('gaussian', g_size, g_sigma);
+
+% Replicate the filter so it is three-dimensional,
 % the total energy does not increase after applying the filter
-h = repmat(h, 1, 1, 3) / 3;
+h = repmat(h, 1, 1, 3);
+
+% Make the frontal and back centre have the same values as the voxels with
+% the same distance in the centre of the filter
+h(:,:,[1,3]) = (h(:,:,[1,3]) / h(2,2,1) ) * h(2, 1, 2);
+
+% Make the sum equal to 1 for the total energy to remain the same
+h = h / sum(h(:));
 
 out_hm = in_hm;
 
