3207e60a2ffe9e37eacdd68ae77bbbc86cfb894b
Garoe <garoedp@gmail.com>
2016-07-04 09:51:07 +0100

    New test with imageSideDistributionInvariant

---
+++ heat_map/test/functions/args_test38.m
@@ -0,0 +1,40 @@
+function [args_path] = args_test38()
+%ARGS_TEST38 Maya-Data1, Real-Fire1 Goal, DistInvariant
+%   ARGS_PATH = ARGS_TEST38() Returns in ARGS_PATH the file path of a .mat
+%   file with arguments defined here.
+%
+%   See also heatMapReconstruction, args_test_template,
+%   args_test_solver_template
+
+%% Change common parameters
+data_dir = fullfile(fileparts(fileparts(mfilename('fullpath'))), 'data');
+args_path = fullfile(data_dir, [mfilename('clas') '.mat']);
+args_test_template(args_path);
+
+L = load(args_path);
+
+goal_img_path = {'~/maya/projects/fire/data/fire-test-pics/real-fire1.jpg'};
+goal_mask_img_path = {'~/maya/projects/fire/data/fire-test-pics/trimap/real-fire1-mask.png'};
+mask_img_path = {'~/maya/projects/fire/images/test102_maya_data/flame-30-mask1.png'};
+raw_file_path = 'data/heat_maps/maya-flame-preset/temperature30-reduced.raw';
+
+scene_name = 'test102_maya_data';
+scene_img_folder = fullfile(L.project_path, 'images', [scene_name '/']);
+
+raw_temp_scale = 600;
+raw_temp_offset = 1200;
+
+error_foo = {@histogramErrorOpti, @imageSideDistributionInvariant};
+prior_fncs = {};
+
+prior_weights = [0.5, 0.5];
+
+options = L.options;
+
+options.CrossoverFcn = @gacrossovercombine;
+options.MutationFcn = @mutationadaptfeasible;
+
+% Save all but L
+save(args_path, '-regexp','^(?!(L|data_dir)$).', '-append');
+
+end
