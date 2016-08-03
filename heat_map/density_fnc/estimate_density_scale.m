4353b0fdbe112d77996318c0e6626963026ac2d7
Garoe <garoedp@gmail.com>
2016-08-03 11:15:09 +0100

    Apply density scale only if using mental ray shader
    In Maya shader scenes there isn't a fire_volume_shader to modify

--- heat_map/density_fnc/estimate_density_scale.m
+++ heat_map/density_fnc/estimate_density_scale.m
@@ -4,49 +4,54 @@ function [ best_density, f_val ] = estimate_density_scale( maya_send, opts, init
 %   [ BEST_DENSITY ] = ESTIMATE_DENSITY_SCALE( MAYA_SEND, OPTS, INIT_HEAT_MAP, ...
 %    FITNESS_FNC, OUTPUT_IMG_FOLDER)
 
-% Save the render images in this folder
-out_dir = fullfile(output_img_folder, 'density-estimates');
-mkdir(out_dir);
-
-j = 1;
-f_val = [];
-
-% Loop for density_scales_range(1) to density_scales_range(2) using the
-% given step size
-i = opts.density_scales_range(1);
-while i <= opts.density_scales_range(2)
-    % Set the new scale
-    opts.maya_new_density_scale = i;
-    maya_set_custom_parameters(maya_send, opts);
+if opts.is_mr == true
+    % Save the render images in this folder
+    out_dir = fullfile(output_img_folder, 'density-estimates');
+    mkdir(out_dir);
     
-    % Evaluate the fitness function
-    f_val(end+1) = fitness_fnc(init_heat_map.v');
+    j = 1;
+    f_val = [];
     
-    % Clear the cache of the fitness as it is saved with the same
-    % temperature
-    clear_cache();
-    
-    % Move the render image to the save folder
-    for k=1:num_goal
-        kstr = num2str(k);
-        movefile(fullfile(output_img_folder, ['current1-Cam' kstr '.tif']), ...
-            fullfile(out_dir, [num2str(j, '%03d') '-density-' num2str(i) ...
-            '-Cam' kstr '.tif']));
+    % Loop for density_scales_range(1) to density_scales_range(2) using the
+    % given step size
+    i = opts.density_scales_range(1);
+    while i <= opts.density_scales_range(2)
+        % Set the new scale
+        opts.maya_new_density_scale = i;
+        maya_set_custom_parameters(maya_send, opts);
+        
+        % Evaluate the fitness function
+        f_val(end+1) = fitness_fnc(init_heat_map.v');
+        
+        % Clear the cache of the fitness as it is saved with the same
+        % temperature
+        clear_cache();
+        
+        % Move the render image to the save folder
+        for k=1:num_goal
+            kstr = num2str(k);
+            movefile(fullfile(output_img_folder, ['current1-Cam' kstr '.tif']), ...
+                fullfile(out_dir, [num2str(j, '%03d') '-density-' num2str(i) ...
+                '-Cam' kstr '.tif']));
+        end
+        
+        i = i * opts.density_scale_inc;
+        j = j + 1;
     end
     
-    i = i * opts.density_scale_inc;
-    j = j + 1;
+    % Get the best density scale, set it and return the value
+    [~, i] = min(f_val);
+    
+    opts.maya_new_density_scale = opts.density_scales_range(1) * ...
+        opts.density_scale_inc^(i - 1);
+    
+    maya_set_custom_parameters(maya_send, opts);
+    
+    best_density = opts.maya_new_density_scale;
+else
+    best_density = [];
+    f_val = [];
 end
-
-% Get the best density scale, set it and return the value
-[~, i] = min(f_val);
-
-opts.maya_new_density_scale = opts.density_scales_range(1) * ...
-    opts.density_scale_inc^(i - 1);
-
-maya_set_custom_parameters(maya_send, opts);
-
-best_density = opts.maya_new_density_scale;
 
 end
 
--- heat_map/utilities/render/maya_set_custom_parameters.m
+++ heat_map/utilities/render/maya_set_custom_parameters.m
@@ -5,20 +5,22 @@ function maya_set_custom_parameters( maya_send, opts )
 %
 %   See also heatMapReconstruction
 
-if isfield(opts, 'maya_density_scale')
-    base_d_scale = opts.maya_density_scale;
-else
-    base_d_scale = 1;
-end
-
-if isfield(opts, 'maya_new_density_scale')
-    
-    for i=1:numel(maya_send)
-        cmd = ['setAttr fire_volume_shader.density_scale ' ...
-            num2str( base_d_scale * opts.maya_new_density_scale)];
-        maya_send{i}(cmd, 0);
+if opts.is_mr == true
+    if isfield(opts, 'maya_density_scale')
+        base_d_scale = opts.maya_density_scale;
+    else
+        base_d_scale = 1;
     end
     
+    if isfield(opts, 'maya_new_density_scale')
+        
+        for i=1:numel(maya_send)
+            cmd = ['setAttr fire_volume_shader.density_scale ' ...
+                num2str( base_d_scale * opts.maya_new_density_scale)];
+            maya_send{i}(cmd, 0);
+        end
+        
+    end
 end
 
 end
