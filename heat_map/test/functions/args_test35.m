2301018d213eb25db97e6559b04196639ad59495
Garoe <garoedp@gmail.com>
2016-08-05 10:35:20 +0100

    Added correct original fire image path to tests 35 and 42

--- heat_map/test/functions/args_test35.m
+++ heat_map/test/functions/args_test35.m
@@ -13,8 +13,9 @@ args_test_template(args_path);
 
 L = load(args_path);
 
-goal_img_path = {'~/maya/projects/fire/data/fire-test-pics/pegoraro1.png'};
-goal_mask_img_path = {'~/maya/projects/fire/data/fire-test-pics/trimap/pegoraro1-mask.png'};
+goal_img_path = {'~/maya/projects/fire/data/fire-test-pics/real-fire1.jpg'};
+goal_mask_img_path = {'~/maya/projects/fire/data/fire-test-pics/trimap/real-fire1-mask.png'};
+in_img_path = {'~/maya/projects/fire/images/test104_maya_render/synthetic1.tif'};
 mask_img_path = {'~/maya/projects/fire/images/test104_maya_render/mask-synthetic1.png'};
 raw_file_path = 'data/heat_maps/maya-flame-preset/temperature30-reduced2.raw';
 
@@ -25,7 +26,7 @@ scene_img_folder = fullfile(L.project_path, 'images', [scene_name '/']);
 is_mr = false; % Using Maya renderer
 
 % Maya temperatures use an normalised scale of [0,1]
-LB = 0; 
+LB = 0;
 UB = 1;
 
 options = L.options;
