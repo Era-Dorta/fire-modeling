9e2b88eaca3341f00e788587423a9fc5fbbacbd1
Garoe <garoedp@gmail.com>
2016-08-09 17:39:38 +0100

    Added support for set density scale for maya fluid

--- heat_map/utilities/render/maya_set_custom_parameters.m
+++ heat_map/utilities/render/maya_set_custom_parameters.m
@@ -5,22 +5,26 @@ function maya_set_custom_parameters( maya_send, opts )
 %
 %   See also heatMapReconstruction
 
-if opts.is_custom_shader == true
+if isfield(opts, 'maya_new_density_scale')
+    
     if isfield(opts, 'maya_density_scale')
         base_d_scale = opts.maya_density_scale;
     else
         base_d_scale = 1;
     end
     
-    if isfield(opts, 'maya_new_density_scale')
-        
-        for i=1:numel(maya_send)
-            cmd = ['setAttr fire_volume_shader.density_scale ' ...
-                num2str( base_d_scale * opts.maya_new_density_scale)];
-            maya_send{i}(cmd, 0);
-        end
-        
+    if opts.is_custom_shader
+        shape_name = 'fire_volume_shader.density_scale';
+    else
+        shape_name = '"Flame:flameShape.densityScale"';
     end
+    
+    for i=1:numel(maya_send)
+        cmd = ['setAttr ' shape_name ' ' ...
+            num2str( base_d_scale * opts.maya_new_density_scale)];
+        maya_send{i}(cmd, 0);
+    end
+    
 end
 
 end
