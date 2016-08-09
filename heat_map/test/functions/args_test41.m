b26ee1b41a046f3543fb1118d236e86194189176
Garoe <garoedp@gmail.com>
2016-08-09 10:54:06 +0100

    Using HSL color space in argst_test41

--- heat_map/test/functions/args_test41.m
+++ heat_map/test/functions/args_test41.m
@@ -9,20 +9,20 @@ function [args_path] = args_test41()
 %% Save with default parameters
 data_dir = fullfile(fileparts(fileparts(mfilename('fullpath'))), 'data');
 args_path = fullfile(data_dir, [mfilename('clas') '.mat']);
-
 args_test_template(args_path);
 
 L = load(args_path);
 
 goal_img_path = {'~/maya/projects/fire/data/fire-test-pics/real-fire1.jpg'};
 goal_mask_img_path = {'~/maya/projects/fire/data/fire-test-pics/trimap/real-fire1-mask.png'};
+in_img_path = {'~/maya/projects/fire/images/test104_maya_render/synthetic1.tif'};
 mask_img_path = {'~/maya/projects/fire/images/test104_maya_render/mask-synthetic1.png'};
 raw_file_path = 'data/heat_maps/maya-flame-preset/temperature30-reduced2.raw';
 
 scene_name = 'test104_maya_render';
 scene_img_folder = fullfile(L.project_path, 'images', [scene_name '/']);
 
-color_space = {'RGB', 'HSV', 'Luv', 'XYZ'};
+color_space = {'RGB', 'HSL', 'Luv'};
 
 is_custom_shader = false;
 
