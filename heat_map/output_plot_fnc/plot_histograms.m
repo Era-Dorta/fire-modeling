ba05b44aa4903ad196024e89446d53f3d14518cf
Garoe <garoedp@gmail.com>
2016-08-10 15:32:16 +0100

    Added plot split for the histograms

--- heat_map/output_plot_fnc/plot_histograms.m
+++ heat_map/output_plot_fnc/plot_histograms.m
@@ -5,14 +5,15 @@ function out_ylim = plot_histograms( n_bins, color_space, is_histo_independent, 
 %   OUTPUT_FOLDER, GOAL_IMGS, GOAL_MASK,  OPTI_IMG, OPTI_MASK)
 
 if isBatchMode()
-    fig_h = figure('Visible', 'off');
+    fig_h = figure('Visible', 'off', [0 0 960 540]);
 else
-    fig_h = figure('Position', [125 500 560 420]);
+    fig_h = figure('Position', [125 500 960 540]);
 end
 
 %% Save histograms for optmized images and goal images
 plot_c = 'rgb';
 edges = linspace(0, 255, n_bins+1);
+plot_max_width = 255;
 
 if nargin == 7
     out_ylim = plot_and_save(in_imgs, in_masks, out_name);
@@ -48,10 +49,42 @@ end
                 end
                 out_ylim{i}(j,:) = ylim();
                 
+                % Save plot
                 if is_histo_independent
                     save_img([img_name '-Cam' istr '-' color_space(j)]);
                 else
                     save_img([img_name '-Cam' istr '-' color_space]);
+                end
+                
+                % Plot is too big, split in chunks
+                n_bins_all = size(hc_img, 2);
+                if n_bins_all > plot_max_width
+                    
+                    k = 1;
+                    while(k < n_bins_all)
+                        k_end = k + plot_max_width;
+                        if k_end > n_bins_all
+                            k_end = n_bins_all;
+                        end
+                        
+                        do_plot(hc_img(j,k:k_end), plot_c(j));
+                        increase_axis_ticks(k-1);
+                        
+                        if nargin == 4
+                            hold on; ylim(in_ylim{i}(j,:)); hold off;
+                        else
+                            hold on; ylim(out_ylim{i}(j,:)); hold off;
+                        end
+                        
+                        if is_histo_independent
+                            save_img([img_name '-Cam' istr '-' color_space(j) ...
+                                '-[' num2str(k) '-' num2str(k_end) ']']);
+                        else
+                            save_img([img_name '-Cam' istr '-' color_space ...
+                                '-[' num2str(k) '-' num2str(k_end) ']']);
+                        end
+                        k = k + plot_max_width + 1;
+                    end
                 end
             end
         end
@@ -70,6 +103,11 @@ end
         function save_img(file_name)
             %saveas(fig_h, fullfile(output_img_folder, file_name), 'tiff');
             saveas(fig_h, fullfile(output_folder, file_name), 'svg');
+        end
+        
+        function increase_axis_ticks(offset)
+            ticks = cellfun(@str2num, get(gca, 'XTickLabel'));
+            set(gca, 'XTickLabel', cellstr(num2str(ticks + offset)));
         end
         
     end
