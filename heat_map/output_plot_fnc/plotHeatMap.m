01622265ca54f64bab91c05d85737d0300f9c559
Garoe <garoedp@gmail.com>
2016-08-10 12:08:55 +0100

    Added scale_factor to plotHeatMap

--- heat_map/output_plot_fnc/plotHeatMap.m
+++ heat_map/output_plot_fnc/plotHeatMap.m
@@ -1,19 +1,41 @@
-function plotHeatMap( heat_map )
-% Plots the heat map given as parameter
+function plotHeatMap( heat_map, scale_factor )
+%PLOTHEATMAP Plots the heat map given as parameter
+%   PLOTHEATMAP( HEAT_MAP ) Plot the heat map
+%
+%   PLOTHEATMAP( HEAT_MAP, SCALE_FACTOR ) Plot the heat map, HEAT_MAP.V are
+%   rescaled before plotting HEAT_MAP.V = HEAT_MAP.V * SCALE_FACTOR
+
 % Do a simple color mapping from the temperatures
 colors = heat_map.v;
-colors = colors / max(colors);
+
+% Apply scale factor if given, also rescale if temperatures are too high,
+% as we are using their values directly and max RGB color is 1
+if nargin == 2 || max(colors) > 1
+    
+    if nargin == 1
+        scale_factor = 1 / max(colors);
+    end
+    
+    colors = colors * scale_factor;
+end
+
 colors = [colors, zeros(heat_map.count, 1), zeros(heat_map.count, 1)];
+
 figure;
 hold on;
+
+scatter3(heat_map.xyz(:,1), heat_map.xyz(:,3), heat_map.xyz(:,2), 1, colors);
+
 set(gca,'xlim', [0, heat_map.size(1)]);
 set(gca,'ylim', [0, heat_map.size(2)]);
 set(gca,'zlim', [0, heat_map.size(3)]);
+
 xlabel('x');
 ylabel('z');
 zlabel('y');
+
 view(-37,20)
-scatter3(heat_map.xyz(:,1), heat_map.xyz(:,3), heat_map.xyz(:,2), 1, colors);
+
 hold off;
 end
 
