40d453c983b60cc0cf674674f6a5c84db4dd7844
Garoe <garoedp@gmail.com>
2016-09-08 10:07:38 +0100

    Added new test functions

---
+++ heat_map/test/functions/args_test51.m
@@ -0,0 +1,44 @@
+function [args_path] = args_test51()
+%ARGS_TEST51 Vox_bin_00841 64 close, Vox_bin_00841 Goal
+%   ARGS_PATH = ARGS_TEST51() Returns in ARGS_PATH the file path of a .mat
+%   file with arguments defined here.
+%
+%   See also heatMapReconstruction, args_test_template,
+%   args_test_solver_template
+
+%% Change common parameters
+data_dir = fullfile(fileparts(fileparts(mfilename('fullpath'))), 'data');
+args_path = fullfile(data_dir, [mfilename('clas') '.mat']);
+args_test_template(args_path);
+
+L = load(args_path);
+
+goal_img_path = {'~/maya/projects/fire/data/from_dmitry/cam1/goal1.png'};
+goal_mask_img_path = {'~/maya/projects/fire/data/from_dmitry/cam1/tri-map.png'};
+in_img_path = {'~/maya/projects/fire/images/test110_like_109_close_camera/test110_like_109_close_camera.tif'};
+in_img_bg_path = {'~/maya/projects/fire/images/test110_like_109_close_camera/background-new.png'};
+mask_img_path = {'~/maya/projects/fire/images/test110_like_109_close_camera/mask.png'};
+raw_file_path = 'data/from_dmitry/vox_bin_00841_clean_64.raw2';
+density_file_path = '~/maya/projects/fire/data/from_dmitry/vox_bin_00841_clean_64.raw2';
+
+scene_name = 'test110_like_109_close_camera';
+scene_img_folder = fullfile(L.project_path, 'images', [scene_name '/']);
+
+error_foo = {@histogramErrorOpti};
+prior_fncs = {};
+add_background = true;
+prior_weights = 1;
+
+add_background = true;
+
+options = L.options;
+
+options.CreationFcn = @gacreationrandom;
+options.CrossoverFcn = @gacrossovercombine2;
+options.MutationFcn = @mutationadaptfeasible;
+density_scales_range = [0.01 1000];
+
+% Save all but L
+save(args_path, '-regexp','^(?!(L|data_dir)$).', '-append');
+
+end
