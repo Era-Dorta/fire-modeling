368d8d09b7618301666a1b53f24d1a445c0acfe6
Garoe <garoedp@gmail.com>
2016-09-23 12:32:36 +0100

    Added new test function

---
+++ heat_map/test/functions/args_test57.m
@@ -0,0 +1,62 @@
+function [args_path] = args_test57()
+%ARGS_TEST57 ICM 32 close, goal-aligned, gaussian, Lab, eval_half
+%   ARGS_PATH = ARGS_TEST57() Returns in ARGS_PATH the file path of a .mat
+%   file with arguments defined here.
+%
+%   See also heatMapReconstruction, args_test_template,
+%   args_test_solver_template
+
+%% Change common parameters
+data_dir = fullfile(fileparts(fileparts(mfilename('fullpath'))), 'data');
+args_path = fullfile(data_dir, [mfilename('clas') '.mat']);
+args_test_template(args_path, false);
+
+solver = 'icm';
+
+% Save all but L
+save(args_path, '-regexp','^(?!(L|data_dir)$).', '-append');
+
+% Update solver parameters
+args_test_solver_template(args_path, solver);
+
+L = load(args_path);
+options = L.options;
+
+goal_img_path = {'~/maya/projects/fire/data/from_dmitry/cam1/goal1-aligned.png'};
+goal_mask_img_path = {'~/maya/projects/fire/data/from_dmitry/cam1/tri-map-aligned.png'};
+in_img_path = {'~/maya/projects/fire/images/test110_like_109_close_camera/test110_like_109_close_camera.tif'};
+in_img_bg_path = {'~/maya/projects/fire/images/test110_like_109_close_camera/background-new.png'};
+mask_img_path = {'~/maya/projects/fire/images/test110_like_109_close_camera/mask.png'};
+raw_file_path = 'data/from_dmitry/vox_bin_00841_clean_32.raw2';
+density_file_path = '~/maya/projects/fire/data/from_dmitry/vox_bin_00841_clean_32.raw2';
+
+scene_name = 'test110_like_109_close_camera';
+scene_img_folder = fullfile(L.project_path, 'images', [scene_name '/']);
+
+error_foo = {@MSEPerceptual};
+prior_fncs = {};
+add_background = true;
+prior_weights = 1;
+
+add_background = true;
+
+density_scales_range = [0.01 1000];
+
+use_cache = false;
+
+color_space = 'Lab';
+
+options.CreateSamplesFcn = @generate_gaussian_temperatures_icm;
+options.UpdateSampleRangeFcn = @update_range_none_icm;
+options.DataTermApproxFcn = {@eval_render_function_half_ite_icm};
+options.DataTermFcn = {@eval_render_function_always_icm};
+options.OutputFcn = [options.OutputFcn, {@gradestimate_density_scale}];
+
+initGuessFnc = @getInitHeatMapScaled_icm;
+
+options.NeighbourhoodSize = 1;
+
+% Save all but L
+save(args_path, '-regexp','^(?!(L|data_dir)$).', '-append');
+
+end
