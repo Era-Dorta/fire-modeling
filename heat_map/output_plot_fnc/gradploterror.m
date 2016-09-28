35dfb0e739d9319bb2f2ac2332f30c6fc029ac5a
Garoe <garoedp@gmail.com>
2016-09-28 14:04:09 +0100

    Fixed out of memory error in gradient plotting

--- heat_map/output_plot_fnc/gradploterror.m
+++ heat_map/output_plot_fnc/gradploterror.m
@@ -10,7 +10,7 @@ end
 
 % Ugly hack to allow for better plotting, as we cannot change the output of
 % the function
-persistent ERROR FIG_H
+persistent ERROR ERROR_SUB X_VAL FIG_H SAMPLE_RATE
 
 % The plot function is called when the algorithm is finished, save the
 % image here
@@ -50,18 +50,44 @@ if optimValues.iteration == 0
     set(gca,'xlim', [0, 1]);
     
     ERROR = optimValues.fval;
+    
+    ERROR_SUB = ERROR;
+    X_VAL = 0:optimValues.iteration;
+    
+    SAMPLE_RATE = 1;
 else
     % Make our figure the current figure
     set(groot, 'CurrentFigure', FIG_H);
     
     ERROR = [ERROR, optimValues.fval];
+    
+    if numel(ERROR) <= 100
+        ERROR_SUB = ERROR;
+        X_VAL = 0:optimValues.iteration;
+    else
+        % Downsample when the number of points is too large, ideally it
+        % would be / 100, for max of 100 points, but due to rounding we
+        % use a lower value
+        new_sample_rate = round(numel(ERROR) / 75);
+        
+        if new_sample_rate ~= SAMPLE_RATE
+            % Clear the plot when the sampling rate changes
+            cla(gca);
+            SAMPLE_RATE = new_sample_rate;
+        end
+        
+        % Down sample the data
+        ERROR_SUB = downsample(ERROR, SAMPLE_RATE);
+        X_VAL = downsample(0:optimValues.iteration, SAMPLE_RATE);
+    end
+    
     set(gca,'xlim', [0, optimValues.iteration]);
 end
 
 hold on;
 
 % Plot the best error
-plot(0:optimValues.iteration, ERROR, '-rx');
+plot(X_VAL, ERROR_SUB, '-rx');
 
 % Legend is a costly plotting function, do it just once
 if optimValues.iteration == 0
