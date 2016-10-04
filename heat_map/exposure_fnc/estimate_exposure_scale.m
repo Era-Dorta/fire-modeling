Era-Dorta/fire-modeling

commit 3f187c3e5d710092d6e561791dd84247cc757cf2

Refactor in estimate exposure code

heat_map/exposure_fnc/estimate_exposure_scale.m
@@ -10,82 +10,19 @@ if ~isempty(opts.exposure_scales_range) && opts.is_custom_shader
         init_heat_map.v(:) = mean([opts.LB, opts.UB]);
     end
     
-    shape_name = 'mia_exposure_photographic1.cm2_factor';
-    
-    % Save the render images in this folder
-    mkdir(out_dir);
-    
     k0 = log10(opts.exposure_scales_range(1));
     k1 = log10(opts.exposure_scales_range(2));
     
     k_samples = linspace(k0, k1, opts.n_exposure_scale);
     
-    f_val = zeros(1, numel(k_samples));
-    
-    maya_par_eval();
-    
-    % Get the best exposure scale, set it and return the value
-    [~, i] = min(f_val);
-    
-    best_exposure = 10^k_samples(i);
-    
-    cmd = ['setAttr ' shape_name ' ' num2str( best_exposure)];
-    for i=1:numel(maya_send)
-        maya_send{i}(cmd, 0);
-    end
+    [best_exposure, f_val] = estimate_exposure_with_range( maya_send, ...
+        opts, init_heat_map, fitness_fnc, output_img_folder, out_dir, ...
+        num_goal, k_samples);
     
 else
     best_exposure = [];
     f_val = [];
 end
-
-    function maya_par_eval
-        num_samples = numel(k_samples);
-        num_maya = min(numel(maya_send), num_samples);
-        x = repmat(init_heat_map.v', num_maya, 1);
-        
-        for j=1:num_maya:num_samples
-            l = j:min(j+num_maya-1, num_samples);
-            
-            for c_maya=1:numel(l)
-                new_exposure = 10^k_samples(l(c_maya));
-                cmd = ['setAttr ' shape_name ' ' num2str(new_exposure)];
-                maya_send{c_maya}(cmd, 0);
-            end
-            
-            f_val(l) = fitness_fnc(x(1:numel(l),:));
-            
-            if num_maya == 1
-                % With one maya instance move each render image to
-                % the save folder
-                for k=1:num_goal
-                    kstr = num2str(k);
-                    movefile(fullfile(output_img_folder, ['current1-Cam' kstr '.tif']), ...
-                        fullfile(out_dir, [num2str(j, '%03d') '-exposure-' ...
-                        num2str(new_exposure) '-Cam' kstr '.tif']));
-                end
-            end
-            
-            % Clear the cache of the fitness as it is saved with the same
-            % temperature
-            if opts.use_cache
-                clear_cache();
-            end
-        end
-        
-        if num_maya > 1
-            % With parallel evaluation we can only move the best image as
-            % the other are deleted inside the fitness_fnc
-            [~, j] = min(f_val);
-            for k=1:num_goal
-                kstr = num2str(k);
-                movefile(fullfile(output_img_folder, ['current1-Cam' kstr '.tif']), ...
-                    fullfile(out_dir, [num2str(j, '%03d') '-exposure-' ...
-                    num2str(10^k_samples(j)) '-Cam' kstr '.tif']));
-            end
-        end
-        
-    end
 
 end
 

heat_map/exposure_fnc/estimate_exposure_with_range.m
@@ -5,18 +5,32 @@ function [ best_exposure, f_val ] = estimate_exposure_with_range( maya_send, ...
 %    FITNESS_FNC, OUTPUT_IMG_FOLDER)
 
 if opts.is_custom_shader
-        
+    
     shape_name = 'mia_exposure_photographic1.cm2_factor';
     
     % Save the render images in this folder
     mkdir(out_dir);
-        
+    
     f_val = zeros(1, numel(k_samples));
+    num_samples = numel(k_samples);
+    num_maya = min(numel(maya_send), num_samples);
+    x = repmat(init_heat_map.v', num_maya, 1);
     
     maya_par_eval();
     
     % Get the best exposure scale, set it and return the value
     [~, i] = min(f_val);
+    
+    if num_maya > 1
+        % With parallel evaluation we can only move the best image as
+        % the other are deleted inside the fitness_fnc
+        for m=1:num_goal
+            mstr = num2str(m);
+            movefile(fullfile(output_img_folder, ['current1-Cam' mstr '.tif']), ...
+                fullfile(out_dir, [num2str(i, '%03d') '-exposure-' ...
+                num2str(k_samples(i)) '-Cam' mstr '.tif']));
+        end
+    end
     
     best_exposure = k_samples(i);
     
@@ -30,10 +44,7 @@ else
     f_val = [];
 end
 
-    function maya_par_eval
-        num_samples = numel(k_samples);
-        num_maya = min(numel(maya_send), num_samples);
-        x = repmat(init_heat_map.v', num_maya, 1);
+    function maya_par_eval()
         
         for j=1:num_maya:num_samples
             l = j:min(j+num_maya-1, num_samples);
@@ -61,18 +72,6 @@ end
             % temperature
             if opts.use_cache
                 clear_cache();
-            end
-        end
-        
-        if num_maya > 1
-            % With parallel evaluation we can only move the best image as
-            % the other are deleted inside the fitness_fnc
-            [~, j] = min(f_val);
-            for k=1:num_goal
-                kstr = num2str(k);
-                movefile(fullfile(output_img_folder, ['current1-Cam' kstr '.tif']), ...
-                    fullfile(out_dir, [num2str(j, '%03d') '-exposure-' ...
-                    num2str(k_samples(j)) '-Cam' kstr '.tif']));
             end
         end
         

commit 65a16b1b0a43b686664f601bf778407ac7e65f2e

Fix bug introduced in 3f187c3e5d710092d6e561791dd84247cc757cf2

heat_map/exposure_fnc/estimate_exposure_scale.m
@@ -13,7 +13,7 @@ if ~isempty(opts.exposure_scales_range) && opts.is_custom_shader
     k0 = log10(opts.exposure_scales_range(1));
     k1 = log10(opts.exposure_scales_range(2));
     
-    k_samples = linspace(k0, k1, opts.n_exposure_scale);
+    k_samples = 10.^linspace(k0, k1, opts.n_exposure_scale);
     
     [best_exposure, f_val] = estimate_exposure_with_range( maya_send, ...
         opts, init_heat_map, fitness_fnc, output_img_folder, out_dir, ...