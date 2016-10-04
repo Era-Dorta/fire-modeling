ab2c681f45cd13f67dc2fc8f7ba268307768fc4b
Garoe <garoedp@gmail.com>
2016-10-04 20:05:23 +0100

    Refactored estimate_density_scale to use estimate_density_with_range

--- heat_map/density_fnc/estimate_density_scale.m
+++ heat_map/density_fnc/estimate_density_scale.m
@@ -5,7 +5,7 @@ function [ best_density, f_val, density_norm ] = estimate_density_scale( maya_se
 %    FITNESS_FNC, OUTPUT_IMG_FOLDER)
 
 if ~isempty(opts.density_scales_range)
-    
+
     if ~isempty(opts.density_file_path)
         density_raw = read_raw_file(opts.density_file_path);
         density_norm = 1 / max(density_raw.v);
@@ -17,53 +17,20 @@ if ~isempty(opts.density_scales_range)
         warning(['If no density file path is given, it is recommended that' ...
             ' the one already set is normalized']);
     end
-    
+
     if use_mean_t
         init_heat_map.v(:) = mean([opts.LB, opts.UB]);
     end
-    
-    % Save the render images in this folder
-    mkdir(out_dir);
+
     
     k0 = log10(opts.density_scales_range(1));
     k1 = log10(opts.density_scales_range(2));
     
-    k_samples = linspace(k0, k1, opts.n_density_scale);
+    k_samples = 10.^linspace(k0, k1, opts.n_density_scale);
     
-    f_val = zeros(1, numel(k_samples));
-    
-    % Loop in a logarithmic scale for the samples
-    for i=1:numel(k_samples)
-        % Set the new scale
-        opts.maya_new_density_scale = 10^k_samples(i) * density_norm;
-        maya_set_custom_parameters(maya_send, opts);
-        
-        % Evaluate the fitness function
-        f_val(i) = fitness_fnc(init_heat_map.v');
-        
-        % Clear the cache of the fitness as it is saved with the same
-        % temperature
-        if opts.use_cache
-            clear_cache();
-        end
-        
-        % Move the render image to the save folder
-        for k=1:num_goal
-            kstr = num2str(k);
-            movefile(fullfile(output_img_folder, ['current1-Cam' kstr '.tif']), ...
-                fullfile(out_dir, [num2str(i, '%03d') '-density-' ...
-                num2str(opts.maya_new_density_scale) '-Cam' kstr '.tif']));
-        end
-    end
-    
-    % Get the best density scale, set it and return the value
-    [~, i] = min(f_val);
-    
-    opts.maya_new_density_scale = 10^k_samples(i) * density_norm;
-    
-    maya_set_custom_parameters(maya_send, opts);
-    
-    best_density = opts.maya_new_density_scale;
+    [best_density, f_val] = estimate_density_with_range( maya_send, ...
+        opts, init_heat_map, fitness_fnc, output_img_folder, out_dir, ...
+        num_goal, k_samples);
     
 else
     best_density = [];
