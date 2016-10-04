a7d07d0811444c54801576e74826fdd5025af6f5
Garoe <garoedp@gmail.com>
2016-10-04 13:51:20 +0100

    Estimate exposure in parallel

--- heat_map/exposure_fnc/estimate_exposure_scale.m
+++ heat_map/exposure_fnc/estimate_exposure_scale.m
@@ -5,7 +5,7 @@ function [ best_exposure, f_val ] = estimate_exposure_scale( maya_send, opts, in
 %    FITNESS_FNC, OUTPUT_IMG_FOLDER)
 
 if ~isempty(opts.exposure_scales_range) && opts.is_custom_shader
-        
+    
     if use_mean_t
         init_heat_map.v(:) = mean([opts.LB, opts.UB]);
     end
@@ -22,39 +22,24 @@ if ~isempty(opts.exposure_scales_range) && opts.is_custom_shader
     
     f_val = zeros(1, numel(k_samples));
     
-    % Loop in a logarithmic scale for the samples
-    for i=1:numel(k_samples)
-        % Set the new scale
-        opts.maya_new_exposure_scale = 10^k_samples(i);
-        cmd = ['setAttr ' shape_name ' ' num2str( opts.maya_new_exposure_scale)];
-        maya_send{1}(cmd, 0);
+    maya_par_eval();
         
-        % Evaluate the fitness function
-        f_val(i) = fitness_fnc(init_heat_map.v');
-        
-        % Clear the cache of the fitness as it is saved with the same
-        % temperature
-        if opts.use_cache
-            clear_cache();
-        end
-        
-        % Move the render image to the save folder
-        for k=1:num_goal
-            kstr = num2str(k);
-            movefile(fullfile(output_img_folder, ['current1-Cam' kstr '.tif']), ...
-                fullfile(out_dir, [num2str(i, '%03d') '-exposure-' ...
-                num2str(opts.maya_new_exposure_scale) '-Cam' kstr '.tif']));
-        end
-    end
-    
     % Get the best exposure scale, set it and return the value
     [~, i] = min(f_val);
+    
+    % Move the render image to the save folder
+    for k=1:num_goal
+        kstr = num2str(k);
+        movefile(fullfile(output_img_folder, ['current1-Cam' kstr '.tif']), ...
+            fullfile(out_dir, [num2str(i, '%03d') '-exposure-' ...
+            num2str(opts.maya_new_exposure_scale) '-Cam' kstr '.tif']));
+    end
     
     opts.maya_new_exposure_scale = 10^k_samples(i);
     
     cmd = ['setAttr ' shape_name ' ' num2str( opts.maya_new_exposure_scale)];
     for i=1:numel(maya_send)
-       maya_send{i}(cmd, 0);
+        maya_send{i}(cmd, 0);
     end
     
     best_exposure = opts.maya_new_exposure_scale;
@@ -63,6 +48,30 @@ else
     best_exposure = [];
     f_val = [];
 end
+
+    function maya_par_eval
+        num_samples = numel(k_samples);
+        num_maya = min(numel(maya_send), num_samples);
+        x = repmat(init_heat_map.v', num_maya, 1);
+        
+        for j=1:num_maya:num_samples
+            l = j:min(j+num_maya-1, num_samples);
+            
+            for c_maya=1:numel(l)
+                opts.maya_new_exposure_scale = 10^k_samples(l(c_maya));
+                cmd = ['setAttr ' shape_name ' ' num2str( opts.maya_new_exposure_scale)];
+                maya_send{c_maya}(cmd, 0);
+            end
+            
+            f_val(l) = fitness_fnc(x(1:numel(l),:));
+
+            % Clear the cache of the fitness as it is saved with the same
+            % temperature
+            if opts.use_cache
+                clear_cache();
+            end
+        end
+    end
 
 end
 
