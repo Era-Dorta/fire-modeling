095adf764d26bf55b4100ce35ab66971a332cc89
Garoe <garoedp@gmail.com>
2016-10-05 18:28:49 +0100

    Added first version of icm re density

---
+++ heat_map/solvers/do_icm_re_density_solve.m
@@ -0,0 +1,73 @@
+function [ heat_map_v, best_error, exitflag] = do_icm_re_density_solve( ...
+    init_heat_map, fitness_foo, paths_str, summary_data, goal_img, goal_mask, ...
+    opts, maya_send)
+% Gradient descent solver for heat map reconstruction
+%% Options for the icm solver
+% Path where the initial population will be saved
+output_data_path = [paths_str.output_folder 'OutputData.mat'];
+
+options = get_icm_options_from_file( opts, init_heat_map,  ...
+    goal_img, goal_mask, output_data_path, paths_str, false, fitness_foo, ...
+    maya_send);
+
+options.exposure = summary_data.best_exposure;
+options.fexposure = min(summary_data.fitness_for_exposure);
+options.density = summary_data.best_density;
+options.fdensity = min(summary_data.fitness_for_density);
+
+LB = ones(init_heat_map.count, 1) * opts.LB;
+UB = ones(init_heat_map.count, 1) * opts.UB;
+
+% Initial guess for ICM
+InitialPopulation = opts.initGuessFnc(init_heat_map, LB', UB');
+
+% Save the initial value
+save(output_data_path, 'InitialPopulation');
+
+%% Call the gradient descent optimization
+startTime = tic;
+
+[heat_map_v, best_error, exitflag, output] = icm_re(fitness_foo, InitialPopulation, ...
+    init_heat_map.xyz, LB, UB, options);
+
+totalTime = toc(startTime);
+disp(['Optimization total time ' num2str(totalTime)]);
+
+%% Save data to file
+FinalScores = best_error;
+FinalPopulation = heat_map_v;
+save(output_data_path, 'FinalPopulation', 'FinalScores', '-append');
+
+%% Visualize distance space
+%visualize_score_space(output_data_path, paths_str.visualization_fig_path);
+
+%% Save summary file
+
+summary_data.OptimizationMethod = 'ICM with resampling';
+summary_data.ImageError = best_error;
+summary_data.HeatMapSize = init_heat_map.size;
+summary_data.HeatMapNumVariables = init_heat_map.count;
+summary_data.OptimizationTime = [num2str(totalTime) ' seconds'];
+summary_data.InitGuessFile = init_heat_map.filename;
+summary_data.OuputDataFile = output_data_path;
+
+save(output_data_path, 'output', '-append');
+
+output.tlr = ['saved in  ' output_data_path];
+output.tur = ['saved in  ' output_data_path];
+
+summary_data.output = output;
+
+% For gradient, options is a class, convert it to struct to use it in the
+% save summary function, the struct() function also copies the private data
+% so copy the public one manually
+fields = fieldnames(summary_data.options);
+for i=1:numel(fields)
+    opt_struct.(fields{i}) = summary_data.options.(fields{i});
+end
+summary_data.options = opt_struct;
+
+save_summary_file(paths_str.summary, summary_data, []);
+
+end
+
---
+++ heat_map/solvers/icm/icm_re_density.m
@@ -0,0 +1,238 @@
+function [ x, fval, exitFlag, output ] = icm_re_density ( fun, x0, xyz, lb, ub, options)
+%ICM_RE Iterative Conditional Modes resampling parallel solver
+
+%% Initialisation
+state = 'init';
+exitFlag = 0;
+num_dim = numel(x0);
+
+% Temperature range, it will be gradually deacreased for each voxel
+tlr = lb;
+tur = ub;
+
+optimValues.fval = 1;
+optimValues.iteration = 0;
+optimValues.funccount = 0;
+optimValues.procedure = 'Initial message';
+optimValues.ite_inc = round(num_dim / 2);
+optimValues.exposure = options.exposure;
+optimValues.fexposure = options.fexposure;
+optimValues.density = options.density;
+optimValues.fdensity = options.fdensity;
+
+if options.DataTermEvalVM > 0.5
+    % More than 50% evaluations, use random generator
+    options.DataTermEvalVMRand = true;
+else
+    % Less than 50% use mod to eval without probabilities
+    options.DataTermEvalVMRand = false;
+    options.DataTermEvalVM = round(1 / options.DataTermEvalVM);
+end
+
+if(options.TemperatureNSamples < 1)
+    exitFlag = -1;
+    optimValues.procedure = 'TemperatureNSamples must be >= 1';
+    disp(optimValues.procedure);
+    return;
+end
+
+% If data term function is eval render, just render once as the
+% values are not changing
+if(isempty(options.DataTermFcn) || (numel(options.DataTermFcn) == 1 ...
+        && ~isempty(strfind(func2str(options.DataTermFcn{1}), ...
+        'eval_render_function_always_icm'))))
+    use_common_dataterm = true;
+else
+    use_common_dataterm = false;
+end
+
+optimValues = options.ExposureFnc(x0, optimValues, state);
+optimValues = options.DensityFnc(x0, optimValues, state);
+
+[~, optimValues] = call_output_fnc_icm(x0, options, optimValues, state);
+
+%% First "iteration" is just an evaluation of the initial point
+% Changing the state and calling twice the output fnc is needed to
+% replicate the behaviour of the other solvers
+state = 'iter';
+
+cur_score = calculate_score_all(x0);
+
+optimValues.fval = mean(cur_score);
+
+% Replicate x to be able to evaluate in parallel
+x = repmat(x0, options.TemperatureNSamples, 1);
+
+optimValues = options.ClusterUpdateFnc(x(1,:), options, optimValues, state);
+[stop, optimValues] = call_output_fnc_icm(x, options, optimValues, state);
+
+display_info_icm(options, optimValues, num_dim);
+
+%% Main loop
+while(~stop)
+    
+    current_score = mean(cur_score);
+    
+    % Iterate for each voxel
+    for i=1:optimValues.ite_inc:num_dim
+        ii = i:min(i+optimValues.ite_inc-1, num_dim);
+        iis = numel(ii);
+        % Get temperature for the current voxel
+        cur_temp = x(1, i);
+        
+        % Assign a different temperature to each copy of x
+        t = options.CreateSamplesFcn(i, x(1,:), optimValues, options, tlr, tur);
+        x(:, ii) = repmat(t', 1, iis);
+        
+        %plotHeatMap( struct('v', x(1,:), 'xyz', xyz, 'size', [32,32,32]), 1/2300, optimValues.iteration == 0 );
+        
+        % Compute all the scores
+        new_score = calculate_score_approx(ii, x);
+        
+        % Get the min
+        [new_score, j] = min(new_score);
+        
+        % Update on improvement
+        if (new_score < cur_score(i))
+            cur_temp = t(j);
+            % Assign the new score to all ii voxels
+            cur_score(ii) = new_score;
+        end
+        
+        % Reset or update the voxel temperature
+        x(:, ii) = cur_temp;
+        
+        [tlr, tur] = options.UpdateSampleRangeFcn(ii, cur_temp, t, tlr, tur);
+        
+    end
+    
+    optimValues = options.ExposureFnc(x(1,:), optimValues, state);
+    optimValues = options.DensityFnc(x(1,:), optimValues, state);
+    
+    % Update the score in case approximations where used
+    cur_score = calculate_score_all(x(1,:));
+    
+    optimValues.fval = mean(cur_score);
+    
+    optimValues.iteration = optimValues.iteration + 1;
+    
+    optimValues = options.ClusterUpdateFnc(x(1,:), options, optimValues, state);
+    
+    [stop, optimValues] = call_output_fnc_icm(x, options, optimValues, state);
+    if (stop)
+        state = 'interrupt';
+        exitFlag = -1;
+    else
+        [stop, optimValues] = check_exit_conditions_icm(options, optimValues, current_score);
+    end
+    
+    display_info_icm(options, optimValues, num_dim);
+end
+
+%% Clean up, exit state
+disp(optimValues.procedure);
+
+state = 'done';
+fval = optimValues.fval;
+
+output = struct('funcCount', optimValues.funccount, 'iterations', ...
+    optimValues.iteration, 'message', optimValues.procedure, 'exposure', ...
+    optimValues.exposure, 'fexposure', optimValues.fexposure, 'density', ...
+    optimValues.density, 'fdensity', optimValues.fdensity,'tlr', tlr, ...
+    'tur', tur);
+
+call_output_fnc_icm(x, options, optimValues, state);
+
+% Remove the copies of x
+x = x(1,:);
+
+%% Auxiliary functions
+    function [score] = calculate_score_approx(i, x)
+        
+        score = data_term_score_approx(i, x);
+        
+        n_i = getNeighborsIndices_icm_re(i, xyz, options.NeighbourhoodSize);
+        
+        score = score + pairwise_term(i, n_i, x);
+        
+    end
+
+    function [score] = data_term_score_approx(i, x)
+        
+        score = zeros(1, size(x, 1));
+        
+        for k=1:numel(options.DataTermFcn)
+            % Call the function with the first voxel index i(1), all the i
+            % have the same temperatures, so they will all share the score
+            % as they are considered to be clustered together, the same
+            % applies to DataTermFcn and PairWiseTermFcn
+            [data_score, optimValues] = options.DataTermApproxFcn{k}(i(1), x,  ...
+                options, optimValues, lb, ub);
+            score = score + data_score * options.DataTermApproxFactors(k);
+        end
+        
+    end
+
+    function [score] = calculate_score(i, x)
+        
+        score = data_term_score(i, x);
+        
+        n_i = getNeighborsIndices_icm_re(i, xyz, options.NeighbourhoodSize);
+        
+        score = score + pairwise_term(i, n_i, x);
+        
+    end
+
+    function [score] = data_term_score(i, x)
+        
+        score = zeros(1, size(x, 1));
+        
+        for k=1:numel(options.DataTermFcn)
+            [data_score, optimValues] = options.DataTermFcn{k}(i(1), x,  ...
+                options, optimValues, lb, ub);
+            score = score + data_score * options.DataTermFactors(k);
+        end
+        
+    end
+
+    function score = pairwise_term(i, n_i, x)
+        
+        score = zeros(1, size(x, 1));
+        
+        for k=1:numel(options.PairWiseTermFcn)
+            score = score + options.PairWiseTermFcn{k}(i(1), n_i, x, options, ...
+                lb, ub) * options.PairWiseTermFactors(k);
+        end
+        
+    end
+
+    function [score] = calculate_score_all(x)
+        
+        score = ones(num_dim, 1);
+        
+        % Compute data term only once for all the temperatures
+        if(use_common_dataterm)
+            
+            [data_score, optimValues] = options.DataTermFcn{1}(1, x,  ...
+                options, optimValues, lb, ub);
+            
+            score(:) = data_score * options.DataTermFactors(1);
+            
+            for k=1:optimValues.ite_inc:num_dim
+                kk = k:min(k+optimValues.ite_inc-1, num_dim);
+                n_i = getNeighborsIndices_icm_re(kk, xyz, options.NeighbourhoodSize);
+                
+                score(kk) = score(kk) + pairwise_term(kk, n_i, x);
+            end
+            
+        else
+            for k=1:optimValues.ite_inc:num_dim
+                kk = k:min(k+optimValues.ite_inc-1, num_dim);
+                score(kk) = calculate_score(kk, x);
+            end
+        end
+        
+    end
+
+end
+
