3498b96cc9053907efa8214823bca5f9b196a5cb
Garoe <garoedp@gmail.com>
2016-10-05 10:44:13 +0100

    Using estimate_density_with_range in gradestimate_density

--- heat_map/output_plot_fnc/gradestimate_density_scale.m
+++ heat_map/output_plot_fnc/gradestimate_density_scale.m
@@ -10,12 +10,18 @@ if strcmp(state,'iter')
     out_dir = fullfile(output_img_folder, 'density-estimates', ['iter-' ...
         num2str(optimValues.iteration + 1)]);
     
-    [best_density, f_val, density_norm] = estimate_density_scale( maya_send, opts, init_heat_map, ...
-        fitness_fnc, output_img_folder, out_dir, num_goal, false);
+    k0 = log10(opts.density_scales_range(1));
+    k1 = log10(opts.density_scales_range(2));
+    
+    k_samples = 10.^linspace(k0, k1, opts.n_density_scale);
+    
+    [best_density, f_val] = estimate_density_with_range( maya_send, ...
+        opts, init_heat_map, fitness_fnc, output_img_folder, out_dir, ...
+        num_goal, k_samples);
     
     if strcmp(opts.options.Display, 'iter')
         disp(['Best density scale ' num2str(best_density) ', fval '  ...
-            num2str(min(f_val)) ', density norm ' num2str(density_norm)]);
+            num2str(min(f_val))]);
     end
 end
 
