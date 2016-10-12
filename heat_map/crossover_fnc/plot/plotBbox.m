1c5011eeb2f3d6c8c6cc5a799dd443fb391208f5
Garoe <garoedp@gmail.com>
2016-10-12 10:24:11 +0100

    Added optional alpha parameter in plotBox function

--- heat_map/crossover_fnc/plot/plotBbox.m
+++ heat_map/crossover_fnc/plot/plotBbox.m
@@ -1,4 +1,4 @@
-function plotBbox( v_min, v_max, color )
+function plotBbox( v_min, v_max, color, alphav )
 %PLOTBBOX Plots a bounding box
 %   PLOTBBOX( V_MIN, V_MAX, COLOR ) Plot a cube (bounding box) with
 %   vertices in V_MIN and V_MAX and COLOR
@@ -12,7 +12,11 @@ fac = [1 2 3 4;
 
 vert = getVertFromBbox(v_min, v_max);
 
-patch('Faces',fac,'Vertices',vert,'FaceColor', color); 
+pb = patch('Faces',fac,'Vertices',vert,'FaceColor', color); 
+
+if nargin == 4
+    alpha(pb, alphav);
+end
 
 end
 
