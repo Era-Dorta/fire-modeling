f7cff74119f3e94250975278dfdce6aedb448bec
Garoe <garoedp@gmail.com>
2017-02-04 16:26:59 +0000

    Check if folder already exists when using /dev/shm/

--- heat_map/utilities/create_test_folder.m
+++ heat_map/utilities/create_test_folder.m
@@ -1,8 +1,12 @@
 function create_test_folder( output_img_folder_name, opts)
 %CREATE_TEST_FOLDER
 if isunix && isfield(opts, 'use_ram_fs') && opts.use_ram_fs
-    % Create a ram file system
+    % Create a RAM file system
     ram_dir = fullfile('/dev/shm/', output_img_folder_name);
+    if(exist(ram_dir, 'dir'))
+        error(['Test folder exists in RAM file system ' ram_dir ...
+            ', delete it or move it to ' opts.scene_img_folder]);
+    end
     mkdir(ram_dir);
     system(['ln -s ' ram_dir ' ' opts.scene_img_folder]);
 else
