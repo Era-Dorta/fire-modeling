0a08706436b3f94d59a3a4244b7c2008f5bddf64
Garoe <garoedp@gmail.com>
2017-03-04 20:28:25 +0000

    Added global timeout to send maya command

--- heat_map/maya_comm/sendMaya.rb
+++ heat_map/maya_comm/sendMaya.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 
 require 'socket'
+require 'timeout'
 
 # maxRetries * 0.1 = number of seconds waiting for Maya to load
 maxRetries = 50
 timesFailed = 0
+maxTimeCommand = 30*60 # If a render takes more than 30 minutes assume it's stuck
 
 if ARGV.size < 2 or ARGV.size > 3
 	puts "Usage : sendMaya <port> <maya command>"
@@ -25,35 +27,37 @@ else
 	failOnreturn = false
 end
 
-while timesFailed <= maxRetries do
-	begin
-		s = TCPSocket.open("localhost", port)
+Timeout::timeout(maxTimeCommand) {
+	while timesFailed <= maxRetries do
+		begin
+			s = TCPSocket.open("localhost", port)
 
-		s.puts(mel)
+			s.puts(mel)
 
-		# Wait for Maya to finish running the command, especially important for
-		# rendering commands that take a lot of time
-		mayaReturn = s.gets()
+			# Wait for Maya to finish running the command, especially important for
+			# rendering commands that take a lot of time
+			mayaReturn = s.gets()
 		
-		# Output the response from Maya, commonly it will be "\n"
-		puts(mayaReturn)
+			# Output the response from Maya, commonly it will be "\n"
+			puts(mayaReturn)
 		
-		# Close the socket
-		s.close
+			# Close the socket
+			s.close
 		
-		# If the render failed a return message will be in read in mayaReturn
-		# but other commands return messages when they execute properly
-		# so add the fail check as a parameter to the script
-		if failOnreturn and mayaReturn != "\n"
-			exit(-1)
+			# If the render failed a return message will be in read in mayaReturn
+			# but other commands return messages when they execute properly
+			# so add the fail check as a parameter to the script
+			if failOnreturn and mayaReturn != "\n"
+				exit(-1)
+			end
+		
+			exit(0)
+		rescue Errno::ECONNREFUSED
+			timesFailed += 1
+			# Maya is not ready yet, wait a bit before retrying
+			sleep(0.1)
 		end
-		
-		exit(0)
-	rescue Errno::ECONNREFUSED
-		timesFailed += 1
-		# Maya is not ready yet, wait a bit before retrying
-		sleep(0.1)
 	end
-end
+}
 
 exit(-1)
